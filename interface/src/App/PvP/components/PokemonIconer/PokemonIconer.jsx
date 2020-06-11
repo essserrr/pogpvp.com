4f083f2221783d88db69d6e1ac8b80d158588d58
Essserrr <blizzz93@gmail.com>
2020-06-11 17:40:39 +0800

    icons rebased

--- interface/src/App/PvP/components/PokemonIconer/PokemonIconer.jsx
+++ interface/src/App/PvP/components/PokemonIconer/PokemonIconer.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 
 const PokemonIconer = React.memo(function (props) {
-    try {
-        var value = require("../../../../icons" + (props.folder ? props.folder : "/pokemons/") + props.src + ".png")
-    }
-    catch (e) {
-        console.log("Icon " + props.src + " not found")
-    }
     return (
-        value && <img
-            src={value}
+        <img
+            src={"/images/icons" + (props.folder ? props.folder : "/pokemons/") + props.src + ".png"}
             className={props.class}
             alt=""
             data-tip data-for={props.for} />
