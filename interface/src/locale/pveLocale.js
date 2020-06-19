5cb0f60104ffa5f9eee6baa0e00dc516ddd169ff
Essserrr <blizzz93@gmail.com>
2020-06-19 19:35:00 +0800

    pve locale first time added

---
+++ interface/src/locale/pveLocale.js
@@ -0,0 +1,43 @@
+export const pveLocale = {
+    en: {
+        tier: "\xa0\xa0\xa0\xa0\xa0\xa0\xa0Raid tier\xa0\xa0\xa0\xa0\xa0\xa0\xa0",
+        tierlist: "Tier",
+
+        aggreasive: "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0Mode\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0",
+        aggrList: {
+            norm: "Normal",
+            aggr: "Aggresive",
+        },
+
+        playernumb: "Players number",
+
+        partysize: "\xa0\xa0\xa0\xa0\xa0Party Size\xa0\xa0\xa0\xa0\xa0",
+
+        weather: "\xa0\xa0\xa0\xa0\xa0\xa0Weather\xa0\xa0\xa0\xa0\xa0\xa0\xa0",
+
+        dodge: "Dodge Strategy",
+
+        friend: "Friendship level",
+    },
+
+    ru: {
+        tier: "\xa0Уровень рейда\xa0",
+        tierlist: "Уровень",
+
+        aggreasive: "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0Режим\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0",
+        aggrList: {
+            norm: "Обычный",
+            aggr: "Агрессивный",
+        },
+
+        playernumb: "\xa0Число игроков\xa0\xa0",
+
+        partysize: "\xa0Размер группы\xa0\xa0",
+
+        weather: "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0Погода\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0",
+
+        dodge: "\xa0\xa0\xa0\xa0\xa0Уклонения\xa0\xa0\xa0\xa0\xa0\xa0",
+
+        friend: "Уровень дружбы",
+    }
+}
