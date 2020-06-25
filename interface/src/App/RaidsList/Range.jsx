f93405703168f6c6cee1acc26f7148fbd37f0cc9
Essserrr <blizzz93@gmail.com>
2020-06-25 21:48:44 +0800

    deleted redundant elements

--- interface/src/App/RaidsList/Range.jsx
+++ interface/src/App/RaidsList/Range.jsx
@@ -3,10 +3,8 @@ import React from "react";
 
 const Range = React.memo(function Pokemon(props) {
     return (
-        <div className={"d-flex justify-content-start p-0 m-0 " + (props.innerClass ? props.innerClass : "")}>
-            <div className={"p-0 m-0 text-center"}>
-                {props.title}{props.left}-{props.right}
-            </div>
+        <div className={(props.innerClass ? props.innerClass : "col-12 text-center p-0 m-0")}>
+            {props.title}{props.left}-{props.right}
         </div>
     )
 });
