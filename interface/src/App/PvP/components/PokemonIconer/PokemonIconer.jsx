166534f97bb209002b306d5e3c2f56b394a0015f
Essserrr <blizzz93@gmail.com>
2020-06-28 19:33:09 +0800

    Pokemon iconer reworked

--- interface/src/App/PvP/components/PokemonIconer/PokemonIconer.jsx
+++ interface/src/App/PvP/components/PokemonIconer/PokemonIconer.jsx
@@ -1,9 +1,32 @@
 import React from "react";
 
 const PokemonIconer = React.memo(function (props) {
+    //if we are looking for a pokemon icon
+    let iconList = [
+        "/images/low/",
+    ]
+    let i = 0
+
+    function addDefaultSrc(ev) {
+        switch (!props.folder) {
+            case true:
+                if (i < iconList.length) {
+                    ev.target.src = iconList[i] + props.src + ".png"
+                    i++
+                } else {
+                    ev.target.src = "/images/missingno.png"
+                }
+                break
+            default:
+                ev.target.src = "/images/missingno.png"
+                break
+        }
+    }
+
     return (
-        <img
+        navigator.userAgent !== "ReactSnap" && <img
             src={"/images" + (props.folder ? props.folder : "/pokemons/") + props.src + ".png"}
+            onError={addDefaultSrc}
             className={props.class}
             alt=""
             data-tip data-for={props.for} />
