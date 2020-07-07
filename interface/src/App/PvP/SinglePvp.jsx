281d0fe81c43204af1afd4c200f538684cec1711
Essserrr <blizzz93@gmail.com>
2020-07-07 17:45:36 +0800

    single pvp code restyled

--- interface/src/App/PvP/SinglePvp.jsx
+++ interface/src/App/PvP/SinglePvp.jsx
@@ -82,37 +82,40 @@ class SinglePvp extends React.PureComponent {
     onNameChange(event, name) {
 
         //get movepool
-        var moves = returnMovePool(event.value, this.props.parentState.pokemonTable, strings.options.moveSelect)
+        let moves = returnMovePool(event.value, this.props.parentState.pokemonTable, strings.options.moveSelect)
         let quick = selectQuick(moves.quickMovePool, this.props.parentState.moveTable, event.value, this.props.parentState.pokemonTable)
         let charge = selectCharge(moves.chargeMovePool, this.props.parentState.moveTable, event.value, this.props.parentState.pokemonTable)
         //create default iv set
-        var ivSet = calculateMaximizedStats(event.value, 40.0, this.props.parentState.pokemonTable)
+        let ivSet = calculateMaximizedStats(event.value, 40.0, this.props.parentState.pokemonTable)
         //set state
         this.setState({
             [name]: {
                 ...this.state[name],
-                "name": event.value,
-                "quickMovePool": moves.quickMovePool,
-                "chargeMovePool": moves.chargeMovePool,
-                "QuickMove": quick,
-                "ChargeMove1": charge.primaryName,
-                "ChargeMove2": charge.secodaryName,
-                "Lvl": ivSet[this.props.parentState.league]["Default"]["Level"],
-                "Atk": ivSet[this.props.parentState.league]["Default"]["Atk"],
-                "Def": ivSet[this.props.parentState.league]["Default"]["Def"],
-                "Sta": ivSet[this.props.parentState.league]["Default"]["Sta"],
-                "AtkStage": "0",
-                "DefStage": "0",
-                "InitialHP": "0",
-                "InitialEnergy": "0",
-                "ivSet": { 40: ivSet },
+                name: event.value,
+                quickMovePool: moves.quickMovePool,
+                chargeMovePool: moves.chargeMovePool,
+                QuickMove: quick,
+                ChargeMove1: charge.primaryName,
+                ChargeMove2: charge.secodaryName,
+                Lvl: ivSet[this.props.parentState.league].Default.Level,
+                Atk: ivSet[this.props.parentState.league].Default.Atk,
+                Def: ivSet[this.props.parentState.league].Default.Def,
+                Sta: ivSet[this.props.parentState.league].Default.Sta,
+                AtkStage: "0",
+                DefStage: "0",
+                InitialHP: "0",
+                InitialEnergy: "0",
+                ivSet: { 40: ivSet },
                 Shields: "0",
                 IsGreedy: "true",
                 IsShadow: "false",
 
-                effAtk: calculateEffStat(event.value, ivSet[this.props.parentState.league]["Default"]["Level"], ivSet[this.props.parentState.league]["Default"]["Atk"], 0, this.props.parentState.pokemonTable, "Atk", "false"),
-                effDef: calculateEffStat(event.value, ivSet[this.props.parentState.league]["Default"]["Level"], ivSet[this.props.parentState.league]["Default"]["Def"], 0, this.props.parentState.pokemonTable, "Def", "false"),
-                effSta: calculateEffStat(event.value, ivSet[this.props.parentState.league]["Default"]["Level"], ivSet[this.props.parentState.league]["Default"]["Sta"], 0, this.props.parentState.pokemonTable, "Sta"),
+                effAtk: calculateEffStat(event.value, ivSet[this.props.parentState.league].Default.Level,
+                    ivSet[this.props.parentState.league].Default.Atk, 0, this.props.parentState.pokemonTable, "Atk", "false"),
+                effDef: calculateEffStat(event.value, ivSet[this.props.parentState.league].Default.Level,
+                    ivSet[this.props.parentState.league].Default.Def, 0, this.props.parentState.pokemonTable, "Def", "false"),
+                effSta: calculateEffStat(event.value, ivSet[this.props.parentState.league].Default.Level,
+                    ivSet[this.props.parentState.league].Default.Sta, 0, this.props.parentState.pokemonTable, "Sta"),
 
                 HP: undefined,
                 Energy: undefined,
@@ -121,9 +124,8 @@ class SinglePvp extends React.PureComponent {
         });
     }
 
-    onInitialStatsChange(event) {
-        var correspondingStat = event.target.name.replace("Initial", '')
-        var role = event.target.getAttribute('attr')
+    onInitialStatsChange(event, role) {
+        let correspondingStat = event.target.name.replace("Initial", '')
         this.setState({
             [role]: {
                 ...this.state[role],
@@ -134,9 +136,9 @@ class SinglePvp extends React.PureComponent {
         });
     }
 
-    onIvChange(event) {
-        var role = event.target.getAttribute('attr')
-        var eff = calculateEffStat(this.state[role].name, this.state[role].Lvl, event.target.value, this.state[role][event.target.name + "Stage"], this.props.parentState.pokemonTable, event.target.name, this.state[role].IsShadow)
+    onIvChange(event, role) {
+        let eff = calculateEffStat(this.state[role].name, this.state[role].Lvl, event.target.value,
+            this.state[role][event.target.name + "Stage"], this.props.parentState.pokemonTable, event.target.name, this.state[role].IsShadow)
         this.setState({
             [role]: {
                 ...this.state[role],
@@ -147,8 +149,7 @@ class SinglePvp extends React.PureComponent {
         });
     }
 
-    onLevelChange(event) {
-        var role = event.target.getAttribute('attr')
+    onLevelChange(event, role) {
         this.setState({
             [role]: {
                 ...this.state[role],
@@ -161,8 +162,7 @@ class SinglePvp extends React.PureComponent {
         });
     }
 
-    onTypeChange(event) {
-        var role = event.target.getAttribute('attr')
+    onTypeChange(event, role) {
         this.setState({
             [role]: {
                 ...this.state[role],
@@ -175,9 +175,8 @@ class SinglePvp extends React.PureComponent {
         });
     }
 
-    onStageChange(event) {
-        var role = event.target.getAttribute('attr')
-        var correspondingStat = event.target.name.replace("Stage", '')
+    onStageChange(event, role) {
+        let correspondingStat = event.target.name.replace("Stage", '')
         this.setState({
             [role]: {
                 ...this.state[role],
@@ -223,6 +222,7 @@ class SinglePvp extends React.PureComponent {
     }
 
     onChange(event, name) {
+        let role = event.target.getAttribute('attr')
         //check if it`s a name change
         if (event.target === undefined) {
             switch (name.name[1]) {
@@ -243,26 +243,25 @@ class SinglePvp extends React.PureComponent {
         }
         //check if it's an initial stat change
         if (event.target.name === "InitialHP" || event.target.name === "InitialEnergy") {
-            this.onInitialStatsChange(event)
+            this.onInitialStatsChange(event, role)
             return
         }
         //check if it's an iv change
         if (event.target.name === "Sta" || event.target.name === "Def" || event.target.name === "Atk") {
-            this.onIvChange(event)
+            this.onIvChange(event, role)
             return
         }
         //check if it's an level change
         if (event.target.name === "Lvl") {
-            this.onLevelChange(event)
+            this.onLevelChange(event, role)
             return
         }
         //if it's an stage change
         if (event.target.name === "AtkStage" || event.target.name === "DefStage") {
-            this.onStageChange(event)
+            this.onStageChange(event, role)
             return
         }
         if (event.target.value === "Select...") {
-            var role = event.target.getAttribute('attr')
             this.setState({
                 [role]: {
                     ...this.state[role],
@@ -274,11 +273,10 @@ class SinglePvp extends React.PureComponent {
         }
         //if it's an type change
         if (event.target.name === "IsShadow") {
-            this.onTypeChange(event)
+            this.onTypeChange(event, role)
             return
         }
         //otherwise follow general pattern
-        role = event.target.getAttribute('attr')
         this.setState({
             [role]: {
                 ...this.state[role],
@@ -291,82 +289,80 @@ class SinglePvp extends React.PureComponent {
     maximizerSubmit(event) {
         event.preventDefault();
 
-        var role = event.target.getAttribute('attr')
-        var level = event.target.getAttribute('level')
-        var stat = event.target.getAttribute('stat')
-        var action = event.target.getAttribute('action')
-        var varIVS = {}
+        let role = event.target.getAttribute('attr')
+        let level = event.target.getAttribute('level')
+        let stat = event.target.getAttribute('stat')
+        let action = event.target.getAttribute('action')
+        let varIVS = {}
 
-        //if there is no data for given level - generate it
-        if (this.state[role]["ivSet"][level] === undefined) {
-            var missingLevel = calculateMaximizedStats(this.state[role].name, level, this.props.parentState.pokemonTable)
-            switch (action) {
-                case "Maximize":
-                    varIVS = missingLevel[this.props.parentState.league][stat]
-                    break
-                default:
-                    varIVS = missingLevel[this.props.parentState.league][action]
-                    break
-            }
-            this.setState({
-                [role]: {
-                    ...this.state[role],
-                    HP: undefined,
-                    Energy: undefined,
 
-                    "Lvl": varIVS.Level,
-                    "Atk": varIVS.Atk,
-                    "Def": varIVS.Def,
-                    "Sta": varIVS.Sta,
-                    effAtk: calculateEffStat(this.state[role].name, varIVS.Level, varIVS.Atk, this.state[role].AtkStage, this.props.parentState.pokemonTable, "Atk", this.state[role].IsShadow),
-                    effDef: calculateEffStat(this.state[role].name, varIVS.Level, varIVS.Def, this.state[role].DefStage, this.props.parentState.pokemonTable, "Def", this.state[role].IsShadow),
-                    effSta: calculateEffStat(this.state[role].name, varIVS.Level, varIVS.Sta, 0, this.props.parentState.pokemonTable, "Sta"),
-                    "ivSet": {
-                        ...this.state[role]["ivSet"],
-                        [level]: missingLevel,
-                    }
-                },
-                stateModified: true,
-            });
-            return
-        }
-        //otherwise just select from the list
-        switch (action) {
-            case "Maximize":
-                varIVS = this.state[role]["ivSet"][level][this.props.parentState.league][stat]
+        switch (this.state[role]["ivSet"][level] === undefined) {
+            //if there is no data for given level - generate it
+            case true:
+                let missingLevel = calculateMaximizedStats(this.state[role].name, level, this.props.parentState.pokemonTable)
+                switch (action) {
+                    case "Maximize":
+                        varIVS = missingLevel[this.props.parentState.league][stat]
+                        break
+                    default:
+                        varIVS = missingLevel[this.props.parentState.league][action]
+                        break
+                }
+                this.setState({
+                    [role]: {
+                        ...this.state[role],
+                        HP: undefined, Energy: undefined,
+                        Lvl: varIVS.Level, Atk: varIVS.Atk, Def: varIVS.Def, Sta: varIVS.Sta,
+                        effAtk: calculateEffStat(this.state[role].name, varIVS.Level, varIVS.Atk, this.state[role].AtkStage,
+                            this.props.parentState.pokemonTable, "Atk", this.state[role].IsShadow),
+                        effDef: calculateEffStat(this.state[role].name, varIVS.Level, varIVS.Def, this.state[role].DefStage,
+                            this.props.parentState.pokemonTable, "Def", this.state[role].IsShadow),
+                        effSta: calculateEffStat(this.state[role].name, varIVS.Level, varIVS.Sta, 0,
+                            this.props.parentState.pokemonTable, "Sta"),
+                        ivSet: {
+                            ...this.state[role].ivSet,
+                            [level]: missingLevel,
+                        }
+                    },
+                    stateModified: true,
+                });
                 break
             default:
-                varIVS = this.state[role]["ivSet"][level][this.props.parentState.league][action]
-                break
+                //otherwise just select from the list
+                switch (action) {
+                    case "Maximize":
+                        varIVS = this.state[role].ivSet[level][this.props.parentState.league][stat]
+                        break
+                    default:
+                        varIVS = this.state[role].ivSet[level][this.props.parentState.league][action]
+                        break
+                }
+                this.setState({
+                    [role]: {
+                        ...this.state[role],
+                        HP: undefined, Energy: undefined,
+                        effAtk: calculateEffStat(this.state[role].name, varIVS.Level, varIVS.Atk, this.state[role].AtkStage,
+                            this.props.parentState.pokemonTable, "Atk", this.state[role].IsShadow),
+                        effDef: calculateEffStat(this.state[role].name, varIVS.Level, varIVS.Def, this.state[role].DefStage,
+                            this.props.parentState.pokemonTable, "Def", this.state[role].IsShadow),
+                        effSta: calculateEffStat(this.state[role].name, varIVS.Level, varIVS.Sta, 0,
+                            this.props.parentState.pokemonTable, "Sta"),
+
+                        Lvl: varIVS.Level, Atk: varIVS.Atk, Def: varIVS.Def, Sta: varIVS.Sta,
+                    },
+                    stateModified: true,
+                });
         }
-        this.setState({
-            [role]: {
-                ...this.state[role],
-                HP: undefined,
-                Energy: undefined,
-
-                effAtk: calculateEffStat(this.state[role].name, varIVS.Level, varIVS.Atk, this.state[role].AtkStage, this.props.parentState.pokemonTable, "Atk", this.state[role].IsShadow),
-                effDef: calculateEffStat(this.state[role].name, varIVS.Level, varIVS.Def, this.state[role].DefStage, this.props.parentState.pokemonTable, "Def", this.state[role].IsShadow),
-                effSta: calculateEffStat(this.state[role].name, varIVS.Level, varIVS.Sta, 0, this.props.parentState.pokemonTable, "Sta"),
-
-                "Lvl": varIVS.Level,
-                "Atk": varIVS.Atk,
-                "Def": varIVS.Def,
-                "Sta": varIVS.Sta,
-            },
-            stateModified: true,
-        });
-
     }
 
     submitForm = async event => {
         //make server pvp request
-        var url = this.props.parentState.league + "/" + encodeQueryData(this.state.attacker) + "/" + encodeQueryData(this.state.defender)
+        let url = this.props.parentState.league + "/" + encodeQueryData(this.state.attacker) + "/" + encodeQueryData(this.state.defender)
         event.preventDefault();
         this.setState({
             loading: true,
         });
-        var reason = ""
+        let reason = ""
         const response = await fetch(((navigator.userAgent !== "ReactSnap") ? process.env.REACT_APP_LOCALHOST : process.env.REACT_APP_PRERENDER) + "/request/single/" + url, {
             method: 'GET',
             headers: {
@@ -374,11 +370,10 @@ class SinglePvp extends React.PureComponent {
                 'Accept-Encoding': 'gzip',
                 'Pvp-Type': this.props.parentState.pvpoke ? "pvpoke" : "normal",
             },
-        })
-            .catch(function (r) {
-                reason = r
-                return
-            });
+        }).catch(function (r) {
+            reason = r
+            return
+        });
         if (reason !== "") {
             this.setState({
                 showResult: false,
@@ -508,7 +503,7 @@ class SinglePvp extends React.PureComponent {
     }
 
     agregateShieldStage(round) {
-        var result = {
+        let result = {
             Attacker: {
                 AtkStage: Number(this.state.attacker.AtkStage),
                 DefStage: Number(this.state.attacker.DefStage),
@@ -640,8 +635,6 @@ class SinglePvp extends React.PureComponent {
 
 
         data.Log = [...this.state.result.Log.slice(0, this.state.constructor.isSelected), ...data.Log.slice(1)]
-
-
         this.setState({
             showResult: true,
             isError: false,
@@ -669,8 +662,7 @@ class SinglePvp extends React.PureComponent {
     }
 
     onClick(event) {
-        var role = event.target.getAttribute('attr')
-
+        let role = event.target.getAttribute('attr')
         if (!(event.target === event.currentTarget) && event.target.getAttribute("name") !== "closeButton") {
             return
         }
