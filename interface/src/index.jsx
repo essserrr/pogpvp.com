31ff19e89767e173c13799af9ecfaf4850991048
Essserrr <blizzz93@gmail.com>
2020-07-25 15:44:23 +0800

    persistent store added

--- interface/src/index.jsx
+++ interface/src/index.jsx
@@ -1,33 +1,39 @@
-import React from "react";
-import ReactDom from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App/App.jsx";
-import "./css/customTheme.scss"
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "bootstrap-css-only/css/bootstrap.min.css";
-import "mdbreact/dist/css/mdb.css";
-
+import React from "react"
+import ReactDom from "react-dom"
+import { BrowserRouter } from "react-router-dom"
+import { PersistGate } from 'redux-persist/lib/integration/react'
 
 import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import { createStore, applyMiddleware } from 'redux'
-import AppReducer from './AppStore/Reducers/Index'
+import { store, persistor } from "./AppStore/Store"
 
-const AppStore = createStore(AppReducer, applyMiddleware(thunk))
+import App from "./App/App.jsx";
 
 
-const rootElement = document.getElementById("root");
+import "./css/customTheme.scss"
+import "@fortawesome/fontawesome-free/css/all.min.css"
+import "bootstrap-css-only/css/bootstrap.min.css"
+import "mdbreact/dist/css/mdb.css"
+
+const rootElement = document.getElementById("root")
 
 if (rootElement.hasChildNodes()) {
     ReactDom.hydrate(
         <BrowserRouter>
-            <Provider store={AppStore}><App /></Provider>
+            <Provider store={store}>
+                <PersistGate loading={null} persistor={persistor}>
+                    <App />
+                </PersistGate>
+            </Provider>
         </BrowserRouter >,
         rootElement);
 } else {
     ReactDom.render(
         <BrowserRouter>
-            <Provider store={AppStore}><App /></Provider>
+            <Provider store={store}>
+                <PersistGate loading={null} persistor={persistor}>
+                    <App />
+                </PersistGate>
+            </Provider>
         </BrowserRouter >,
         rootElement);
 }
