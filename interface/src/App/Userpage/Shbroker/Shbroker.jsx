54e8375153d6ade0df8d41005ae4b07241ec4953
Essserrr <blizzz93@gmail.com>
2020-07-25 21:01:10 +0800

    new functions sketches added

---
+++ interface/src/App/Userpage/Shbroker/Shbroker.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { getCookie } from "../../../js/getCookie"
+
+import LocalizedStrings from "react-localization";
+import { locale } from "../../../locale/locale"
+
+
+let strings = new LocalizedStrings(locale);
+
+
+class Root extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        strings.setLanguage(getCookie("appLang") ? getCookie("appLang") : "en")
+    }
+
+    render() {
+
+        return (
+            <div className="col pt-2 px-2">
+                <div className="col px-0">
+                    Some Shbroker native stuff
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Root
