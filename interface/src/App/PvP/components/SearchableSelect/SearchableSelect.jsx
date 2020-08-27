ce6c051343fef70b8a93b8a73e8c8271ea3d4d03
Essserrr <blizzz93@gmail.com>
2020-08-27 19:49:33 +0900

    Searchable select styles moved to a separate file

--- interface/src/App/PvP/components/SearchableSelect/SearchableSelect.jsx
+++ interface/src/App/PvP/components/SearchableSelect/SearchableSelect.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import Select from "react-select";
 
+import "./SearchableSelect.scss"
+
 const SearchableSelect = React.memo(function (props) {
-    const customStyles = {
-        control: styles => ({ ...styles, borderColor: "#59698d", borderWidth: "0.8px" }),
-    }
+
     return (
         <Select
-            className="font90 modifiedBorder"
-            classNamePrefix="modifiedBorder"
+            className={props.class ? props.class : ""}
+            classNamePrefix={props.classPrefix ? props.classPrefix : "searchable-select "}
             value={{ value: props.value, label: props.value }}
             options={props.list}
             onChange={props.onChange}
             name={[props.attr, props.category]}
-            styles={customStyles}
         />
     )
 
