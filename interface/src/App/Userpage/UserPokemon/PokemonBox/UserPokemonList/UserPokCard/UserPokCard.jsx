b835fbe71bd5e9de37517214fb6c299aff3fbc58
Essserrr <blizzz93@gmail.com>
2020-09-01 10:29:30 +0900

    Pokemon cards are finished

--- interface/src/App/Userpage/UserPokemon/PokemonBox/UserPokemonList/UserPokCard/UserPokCard.jsx
+++ interface/src/App/Userpage/UserPokemon/PokemonBox/UserPokemonList/UserPokCard/UserPokCard.jsx
@@ -9,25 +9,36 @@ import { ReactComponent as Shadow } from "../../../../../../icons/shadow.svg"
 import "./UserPokCard.scss"
 
 const UserPokCard = React.memo(function (props) {
-    console.log(props)
+    console.log(props.moveTable[props.value.QuickMove])
+    console.log(props.moveTable[props.value.ChargeMove])
     return (
-        <div className="ushinycard col-auto my-1 mx-1 d-flex align-items-center">
-            <ReactTooltip
-                className={"infoTip"}
-                id={props.attr + props.value.Name} effect="solid"
-                place={"top"}
-                multiline={true} >
-                {props.value.Name}
-            </ReactTooltip>
-            <div data-tip data-for={props.attr + props.value.Name} className="col-auto p-0 posRel">
-                {props.pokemonTable[props.value.Name] && <PokemonIconer
-                    src={props.pokemonTable[props.value.Name].Number +
-                        (props.pokemonTable[props.value.Name].Forme !== "" ? "-" + props.pokemonTable[props.value.Name].Forme : "")}
-                    class={"icon36 mr-1"}
-                />}
-                {props.value.IsShadow === "true" ? <Shadow className="posAbs icon18" style={{ right: "-3px" }} /> : null}
+        <div className="upokcard col-auto p-1 m-1 text-center">
+            <div className="mx-1 mb-1 row align-items-center">
+                <ReactTooltip
+                    className={"infoTip"}
+                    id={props.attr + props.value.Name} effect="solid"
+                    place={"top"}
+                    multiline={true} >
+                    {props.value.Name}
+                </ReactTooltip>
+                <div data-tip data-for={props.attr + props.value.Name} className="col-auto px-0 posRel">
+                    {props.pokemonTable[props.value.Name] && <PokemonIconer
+                        src={props.pokemonTable[props.value.Name].Number +
+                            (props.pokemonTable[props.value.Name].Forme !== "" ? "-" + props.pokemonTable[props.value.Name].Forme : "")}
+                        class={"icon36 mr-1"}
+                    />}
+                    {props.value.IsShadow === "true" ? <Shadow className="posAbs icon18" style={{ right: "-3px" }} /> : null}
+                </div>
+                <div className="col px-2">{`${props.value.Lvl}/${props.value.Atk}/${props.value.Def}/${props.value.Sta}`}</div>
+                {props.onClick && <CloseButton attr={props.attr} index={props.index} className="close" onClick={props.onClick} />}
             </div>
-            {props.onClick && <CloseButton attr={props.attr} index={props.index} className="close" onClick={props.onClick} />}
+
+            <div className={"col-12 mb-1  moveStyle typeColorC" + props.moveTable[props.value.QuickMove].MoveType + " text"} >
+                {props.value.QuickMove}
+            </div>
+            <div className={"col-12  moveStyle typeColorC" + props.moveTable[props.value.ChargeMove].MoveType + " text"}>
+                {props.value.ChargeMove}
+            </div>
         </div>
     )
 })
