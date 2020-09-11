795d10fe6175dab63d07dc99c40b0878c9c4548d
Essserrr <blizzz93@gmail.com>
2020-09-11 17:37:06 +0900

    searchable select has label now

--- interface/src/App/PvP/components/SearchableSelect/SearchableSelect.jsx
+++ interface/src/App/PvP/components/SearchableSelect/SearchableSelect.jsx
@@ -6,14 +6,17 @@ import "./SearchableSelect.scss"
 const SearchableSelect = React.memo(function (props) {
 
     return (
-        <Select
-            className={props.class ? props.class : ""}
-            classNamePrefix={props.classPrefix ? props.classPrefix : "searchable-select "}
-            value={{ value: props.value, label: props.value }}
-            options={props.list}
-            onChange={props.onChange}
-            name={[props.attr, props.category]}
-        />
+        <>
+            {props.label && <div className="searchable-select__text col-12 px-0">{props.label}</div>}
+            <Select
+                className={props.class ? props.class : ""}
+                classNamePrefix={props.classPrefix ? props.classPrefix : "searchable-select "}
+                value={{ value: props.value, label: props.value }}
+                options={props.list}
+                onChange={props.onChange}
+                name={[props.attr, props.category]}
+            />
+        </>
     )
 
 });
