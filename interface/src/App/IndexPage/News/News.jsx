783f6267de92ea84f7f0f5108372bb7d8f265eeb
Essserrr <blizzz93@gmail.com>
2020-09-12 22:47:11 +0900

    single news styles moved to a separate file

--- interface/src/App/IndexPage/News/News.jsx
+++ interface/src/App/IndexPage/News/News.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React from "react"
 
+import "./News.scss"
 
 const News = React.memo(function (props) {
     return (
         <div className={props.class}>
-            <div className="singleNewsTitle px-3 py-1">
+            <div className="news__title px-3 py-1">
                 {props.title + "  " + props.date}
             </div>
-            {<div className="singleNewsBody mx-3 pt-3" dangerouslySetInnerHTML={{ __html: props.description }} />}
+            {<div className="news__body mx-3 pt-3" dangerouslySetInnerHTML={{ __html: props.description }} />}
         </div>
     )
 });
