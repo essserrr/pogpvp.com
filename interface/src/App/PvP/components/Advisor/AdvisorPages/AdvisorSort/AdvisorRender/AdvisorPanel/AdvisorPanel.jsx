2d4160428460517fa5896552d004d62a77c921c6
Essserrr <blizzz93@gmail.com>
2020-09-13 23:40:32 +0900

    Advisor panel stylesheet modified

--- interface/src/App/PvP/components/Advisor/AdvisorPages/AdvisorSort/AdvisorRender/AdvisorPanel/AdvisorPanel.jsx
+++ interface/src/App/PvP/components/Advisor/AdvisorPages/AdvisorSort/AdvisorRender/AdvisorPanel/AdvisorPanel.jsx
@@ -31,46 +31,46 @@ class AdvisorPanel extends React.PureComponent {
 
     render() {
         return (
-            <div className={"pokCard bigFont row m-0 py-1 justify-content-between"}>
+            <div className={"advisor-panel__card row m-0 py-1 justify-content-between"}>
                 <div className={"col d-flex p-0"}>
 
                     <span className="mx-2 align-self-center ">{"#" + (this.props.i + 1)}</span>
 
-                    <div className="col-auto px-0 mx-1 mx-sm-2 text-center advisorbody--minwidth">
-                        <div className="col-auto px-0 posRel">
-                            {String(this.props.first.IsShadow) === "true" && <Shadow className="posAbs icon16" />}
+                    <div className="advisor-panel__body--minwidth col-auto px-0 mx-1 mx-sm-2 text-center">
+                        <div className="advisor-panel--relative col-auto px-0">
+                            {String(this.props.first.IsShadow) === "true" && <Shadow className="advisor-panel__shadow-icon" />}
                             <PokemonIconer
                                 src={this.props.pokemonTable[this.props.first.name].Number + (this.props.pokemonTable[this.props.first.name].Forme !== "" ? "-" + this.props.pokemonTable[this.props.first.name].Forme : "")}
-                                class={"icon48"} />
+                                class={"advisor-panel__icon"} />
 
                         </div>
-                        <div className="col-auto px-0 advisorbody__text">
+                        <div className="advisor-panel__body--text col-auto px-0">
                             {getAbbriviation(this.props.first.QuickMove, this.props.first.ChargeMove1, this.props.first.ChargeMove2,
                                 this.props.first.name, this.props.pokemonTable)}
                         </div>
                     </div>
 
-                    <div className="col-auto px-0 mx-1 mx-sm-2 text-center advisorbody--minwidth">
-                        <div className="col-auto px-0 posRel">
-                            {String(this.props.first.IsShadow) === "true" && <Shadow className="posAbs icon16" />}
+                    <div className="advisor-panel__body--minwidth col-auto px-0 mx-1 mx-sm-2 text-center">
+                        <div className="advisor-panel--relative col-auto px-0">
+                            {String(this.props.second.IsShadow) === "true" && <Shadow className="advisor-panel__shadow-icon" />}
                             <PokemonIconer
                                 src={this.props.pokemonTable[this.props.second.name].Number + (this.props.pokemonTable[this.props.second.name].Forme !== "" ? "-" + this.props.pokemonTable[this.props.second.name].Forme : "")}
-                                class={"icon48"} />
+                                class={"advisor-panel__icon"} />
                         </div>
-                        <div className="col-auto px-0 advisorbody__text">
+                        <div className="advisor-panel__body--text col-auto px-0">
                             {getAbbriviation(this.props.second.QuickMove, this.props.second.ChargeMove1, this.props.second.ChargeMove2,
                                 this.props.second.name, this.props.pokemonTable)}
                         </div>
                     </div>
 
-                    <div className="col-auto px-0 mx-1 mx-sm-2 text-center advisorbody--minwidth">
-                        <div className="col-auto px-0 posRel">
-                            {String(this.props.first.IsShadow) === "true" && <Shadow className="posAbs icon16" />}
+                    <div className="advisor-panel__body--minwidth col-auto px-0 mx-1 mx-sm-2 text-center">
+                        <div className="advisor-panel--relative col-auto px-0">
+                            {String(this.props.third.IsShadow) === "true" && <Shadow className="advisor-panel__shadow-icon" />}
                             <PokemonIconer
                                 src={this.props.pokemonTable[this.props.third.name].Number + (this.props.pokemonTable[this.props.third.name].Forme !== "" ? "-" + this.props.pokemonTable[this.props.third.name].Forme : "")}
-                                class={"icon48"} />
+                                class={"advisor-panel__icon"} />
                         </div>
-                        <div className="col-auto px-0 advisorbody__text">
+                        <div className="advisor-panel__body--text col-auto px-0">
                             {getAbbriviation(this.props.third.QuickMove, this.props.third.ChargeMove1, this.props.third.ChargeMove2,
                                 this.props.third.name, this.props.pokemonTable)}
                         </div>
@@ -90,7 +90,7 @@ class AdvisorPanel extends React.PureComponent {
                 <i onClick={this.onClick}
                     className={"clickable align-self-center mr-3 " +
                         (this.state.showCollapse ? "fas fa-angle-up fa-lg " : "fas fa-angle-down fa-lg")}></i>
-                <div className={"col-12 p-0 " + (this.state.showCollapse ? "borderTop" : "")}>
+                <div className={`col-12 p-0 ${this.state.showCollapse ? "advisor-panel--border-top" : ""}`}>
                     <UnmountClosed isOpened={this.state.showCollapse}>
                         {this.state.colElement}
                     </UnmountClosed>
