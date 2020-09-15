14782d581c00a742d07f13b2d95f6a34786f1986
Essserrr <blizzz93@gmail.com>
2020-09-15 12:08:07 +0900

    list element stylesheet added

--- interface/src/App/Movedex/MoveCard/UsesList/ListElement.jsx
+++ interface/src/App/Movedex/MoveCard/UsesList/ListElement.jsx
@@ -2,11 +2,11 @@ import React from "react"
 import LocalizedStrings from "react-localization"
 import { Link } from "react-router-dom"
 
-
 import { getCookie } from "../../../../js/getCookie"
 import { dexLocale } from "../../../../locale/dexLocale"
 import PokemonIconer from "../../../PvP/components/PokemonIconer/PokemonIconer"
 
+import "./ListElement.scss"
 
 let strings = new LocalizedStrings(dexLocale)
 
@@ -17,10 +17,10 @@ const ListElement = React.memo(function (props) {
         <div className="col-6 col-sm-4 col-md-3 p-0">
             <PokemonIconer
                 src={props.value.Number + (props.value.Forme !== "" ? "-" + props.value.Forme : "")}
-                class={"icon36 mr-2"}
+                class={"uses-listelem__icon mr-2"}
             />
             <Link title={strings.dexentr + props.name}
-                className="dexFont link"
+                className="uses-listelem__link"
                 to={(navigator.userAgent === "ReactSnap") ? "/" : "/pokedex/id/" + encodeURIComponent(props.name)}>
                 {props.name}
             </Link>
