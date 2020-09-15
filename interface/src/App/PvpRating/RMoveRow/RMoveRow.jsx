324ec6f9da54066dcebf563b52535e754109c9f1
Essserrr <blizzz93@gmail.com>
2020-09-15 22:58:38 +0900

    rating row stylesheets added

--- interface/src/App/PvpRating/RMoveRow/RMoveRow.jsx
+++ interface/src/App/PvpRating/RMoveRow/RMoveRow.jsx
@@ -1,14 +1,15 @@
 import React from "react"
 import ShortMove from "./ShortMove/ShortMove"
 
+import "./RMoveRow.scss"
+
 const RMoveRow = React.memo(function (props) {
     function addStar(moveName) {
         return (props.pokemonTable[props.pokName].EliteMoves[moveName] === 1 ? "*" : "")
-
     }
 
     return (
-        <div className="row justify-content-between styleRating fBolder animRating mb-1 mx-2 mx-md-3">
+        <div className="rating-moverow row justify-content-between mb-1 mx-2 mx-md-3">
             <div className="col p-0">
                 <div className="row justify-content-md-left m-0 p-0">
                     {props.moveTable[props.value.Quick] &&
--- interface/src/App/PvpRating/RRateRow/RRateRow.jsx
+++ interface/src/App/PvpRating/RRateRow/RRateRow.jsx
@@ -1,19 +1,21 @@
-import React from "react";
+import React from "react"
 import PokemonIconer from "../../PvP/components/PokemonIconer/PokemonIconer"
-import { ReactComponent as Shadow } from "../../../icons/shadow.svg";
+import { ReactComponent as Shadow } from "../../../icons/shadow.svg"
 import { Link } from "react-router-dom"
+
+import "./RRateRow.scss"
 
 const RRateRow = React.memo(function (props) {
     return (
         <Link style={{ color: "black" }} key={props.value.Name} name={props.pokName} to={props.onClickRedirect(props.value.Name)}
-            className="row styleRating fBolder clickable animRating justify-content-between px-2 mb-1 mx-2 mx-md-3">
-            <div className="col p-0 posRel">
+            className="rating-raterow row justify-content-between px-2 mb-1 mx-2 mx-md-3">
+            <div className="col p-0">
                 <PokemonIconer
                     src={props.pokemonTable[props.pokName].Number +
                         (props.pokemonTable[props.pokName].Forme !== "" ? "-" + props.pokemonTable[props.pokName].Forme : "")}
-                    class={"icon24 mr-1 d-inline"} />
+                    class={"rating-raterow__icon mr-1 d-inline"} />
                 {props.pokName}
-                {(props.pokName !== props.value.Name) && <Shadow className="allign-self-center icon24 py-1 ml-1" />}
+                {(props.pokName !== props.value.Name) && <Shadow className="rating-raterow__icon allign-self-center py-1 ml-1" />}
             </div>
             <div className="col-auto p-0">
                 {props.value.Rate}
