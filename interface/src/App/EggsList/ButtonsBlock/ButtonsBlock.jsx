3097a765bf7b7704e1bf3f917bea63f363f103df
Essserrr <blizzz93@gmail.com>
2020-09-16 11:44:32 +0900

    bouble slider, eggs slider stylesheets added

--- interface/src/App/EggsList/ButtonsBlock/ButtonsBlock.jsx
+++ interface/src/App/EggsList/ButtonsBlock/ButtonsBlock.jsx
@@ -6,49 +6,50 @@ import Button from "../../Movedex/MoveCard/DoubleSlider/Button/Button"
 import { locale } from "../../../locale/locale"
 import { getCookie } from "../../../js/getCookie"
 
-let strings = new LocalizedStrings(locale);
+import "./ButtonsBlock.scss"
 
+let strings = new LocalizedStrings(locale)
 
 const ButtonsBlock = React.memo(function (props) {
     strings.setLanguage(getCookie("appLang") ? getCookie("appLang") : "en")
 
     return (
 
-        <div className={"row m-0 mb-3 text-center sliderGroup justify-content-center"} >
+        <div className={"eggsslider-group row m-0 mb-3 text-center justify-content-center"} >
             <Button
                 attr="eggs0"
                 title={"10 km"}
-                class={props.filter.eggs0 ? "col py-1 sliderButton active" : "col py-1 sliderButton"}
+                class={props.filter.eggs0 ? "eggsslider-group__button active col py-1 " : "eggsslider-group__button col py-1"}
                 onClick={props.onFilter}
             />
             <Button
                 attr="eggs1"
                 title={"7 km"}
-                class={props.filter.eggs1 ? "col py-1 sliderButton active" : "col py-1 sliderButton"}
+                class={props.filter.eggs1 ? "eggsslider-group__button active col py-1 " : "eggsslider-group__button col py-1"}
                 onClick={props.onFilter}
             />
             <Button
                 attr={"eggs2"}
                 title={"5 km"}
-                class={props.filter.eggs2 ? "col py-1 sliderButton active" : "col py-1 sliderButton"}
+                class={props.filter.eggs2 ? "eggsslider-group__button active col py-1 " : "eggsslider-group__button col py-1"}
                 onClick={props.onFilter}
             />
             <Button
                 attr={"eggs3"}
                 title={"2 km"}
-                class={props.filter.eggs3 ? "col py-1 sliderButton active" : "col py-1 sliderButton"}
+                class={props.filter.eggs3 ? "eggsslider-group__button active col py-1 " : "eggsslider-group__button col py-1"}
                 onClick={props.onFilter}
             />
             <Button
                 attr={"eggs4"}
                 title={"10 km (50 km)"}
-                class={props.filter.eggs4 ? "col py-1 sliderButton active" : "col py-1 sliderButton"}
+                class={props.filter.eggs4 ? "eggsslider-group__button active col py-1 " : "eggsslider-group__button col py-1"}
                 onClick={props.onFilter}
             />
             <Button
                 attr={"eggs5"}
                 title={"5 km (25 km)"}
-                class={props.filter.eggs5 ? "col py-1 sliderButton active" : "col py-1 sliderButton"}
+                class={props.filter.eggs5 ? "eggsslider-group__button active col py-1 " : "eggsslider-group__button col py-1"}
                 onClick={props.onFilter}
             />
         </div>
--- interface/src/App/Movedex/MoveCard/DoubleSlider/DoubleSlider.jsx
+++ interface/src/App/Movedex/MoveCard/DoubleSlider/DoubleSlider.jsx
@@ -1,19 +1,23 @@
 import React from "react"
 import Button from "./Button/Button"
 
+import "./DoubleSlider.scss"
+
 const DoubleSlider = React.memo(function (props) {
     return (
-        <div className={"row m-0 my-2 text-center sliderGroup justify-content-center"} >
+        <div className={"doubleslider-group row m-0 my-2 text-center justify-content-center"} >
             <Button
                 attr={props.attr1}
                 title={props.title1}
-                class={props.active1 ? "col py-1 sliderButton active" : "col py-1 sliderButton"}
+                class={props.active1 ?
+                    "doubleslider-group__button active col py-1" : "doubleslider-group__button col py-1"}
                 onClick={props.onClick}
             />
             <Button
                 attr={props.attr2}
                 title={props.title2}
-                class={props.active2 ? "col py-1 sliderButton active" : "col py-1 sliderButton"}
+                class={props.active2 ?
+                    "doubleslider-group__button active col py-1" : "doubleslider-group__button col py-1"}
                 onClick={props.onClick}
             />
         </div>
