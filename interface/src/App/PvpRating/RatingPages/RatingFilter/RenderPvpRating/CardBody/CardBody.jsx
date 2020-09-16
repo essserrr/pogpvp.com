a038c6a767661912affcc496453ca511c2bf3206
Essserrr <blizzz93@gmail.com>
2020-09-16 10:25:30 +0900

    rating card body styleahseet added

--- interface/src/App/PvpRating/RatingPages/RatingFilter/RenderPvpRating/CardBody/CardBody.jsx
+++ interface/src/App/PvpRating/RatingPages/RatingFilter/RenderPvpRating/CardBody/CardBody.jsx
@@ -1,38 +1,38 @@
 import React from "react"
 import LocalizedStrings from "react-localization"
 
-
 import Type from "../../../../../PvP/components/CpAndTypes/Type"
 import { getCookie } from "../../../../../../js/getCookie"
-
 import { locale } from "../../../../../../locale/locale"
 
-let strings = new LocalizedStrings(locale);
+import "./CardBody.scss"
+
+let strings = new LocalizedStrings(locale)
 
 const CardBody = React.memo(function (props) {
     strings.setLanguage(getCookie("appLang") ? getCookie("appLang") : "en")
 
 
     return (
-        <div className="row justify-content-between m-0">
-            <div className="col dexFont p-0">
-                <div className="row m-0">
-                    <span className="align-self-center  mr-2">
+        <div className="row justify-content-between align-items-center m-0">
+            <div className="ratingcard-body--font col p-0">
+                <div className="row m-0 align-items-center">
+                    <span className="mr-2">
                         {strings.rating.type}
                     </span>
                     {(props.pokemonTable[props.name].Type[0] !== undefined) && <Type
-                        class={"icon18"}
+                        class={"ratingcard-body__icon"}
                         code={props.pokemonTable[props.name].Type[0]}
                     />}
                     {(props.pokemonTable[props.name].Type[1] !== undefined) && <Type
-                        class={"ml-2 icon18"}
+                        class={"ratingcard-body__icon ml-2"}
                         code={props.pokemonTable[props.name].Type[1]}
                     />}
                 </div>
                 {strings.rating.avgRate + " " + props.entry.AvgRate} <br />
                 {strings.rating.avgWin + " " + (props.entry.AvgWinrate * 100).toFixed(0) + "%"}
             </div>
-            <div className="col-auto styleRating text-center align-self-center px-2 mx-2" style={{ fontSize: "1.2rem", fontWeight: "bold" }}>
+            <div className="ratingcard-body__score col-auto text-center px-2 mx-2">
                 {strings.rating.score}<br />
                 {(props.entry.AvgRateWeighted / props.maxWeighted * 100).toFixed(1)}
             </div>
