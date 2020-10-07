fbfdf87963cf43c7d603927cc55430c905e93ba0
Essserrr <blizzz93@gmail.com>
2020-10-07 21:25:31 +0900

    optinal rendering added

--- interface/src/App/PvE/Components/PveResult/PveResListFilter/PveResListSort/PveResListRender/PveResEntry/FightStats/FightStats.jsx
+++ interface/src/App/PvE/Components/PveResult/PveResListFilter/PveResListSort/PveResListRender/PveResEntry/FightStats/FightStats.jsx
@@ -1,33 +1,44 @@
-import React from "react";
+import React from "react"
+import LocalizedStrings from "react-localization"
 
+import { getCookie } from "../../../../../../../../../js/getCookie"
+import { pveLocale } from "../../../../../../../../../locale/pveLocale"
+
+let pveStrings = new LocalizedStrings(pveLocale)
 
 const FightStats = React.memo(function (props) {
+    pveStrings.setLanguage(getCookie("appLang") ? getCookie("appLang") : "en")
+
     return (
         <div className="row m-0 fBolder">
-            <div className="col-6 p-0 pr-1">
+            {props.dAvg !== undefined && <div className="col-6 p-0 pr-1">
                 <i className="fas fa-crosshairs mr-1"></i>
-                {props.dAvg + "% (" + props.dMin + "% - " + props.dMax + "%)"}
-            </div>
-            <div className="col-6 p-0">
+                {`${props.dAvg}%`}
+                {props.dMin !== undefined && props.dMax !== undefined && ` (${props.dMin}% - ${props.dMax}%)`}
+            </div>}
+            {props.plAvg !== undefined && <div className="col-6 p-0">
                 <i className="fas fa-users mr-1"></i>
-                {props.plAvg + " (" + props.plMin + " - " + props.plMax + ")"}
-            </div>
-            <div className="col-6 p-0 pr-1">
+                {`${props.plAvg}`}
+                {props.plMin !== undefined && props.plMax !== undefined && ` (${props.plMin} - ${props.plMax})`}
+            </div>}
+            {props.dpsAvg !== undefined && <div className="col-6 p-0 pr-1">
                 <i className="fab fa-cloudscale mr-1"></i>
-                {props.dpsAvg + " (" + props.dpsMin + " - " + props.dpsMax + ")"}
-            </div>
-            <div className="col-6 p-0">
+                {`${props.dpsAvg}`}
+                {props.dpsMin !== undefined && props.dpsMax !== undefined && ` (${props.dpsMin} - ${props.dpsMax})`}
+            </div>}
+            {props.FMin !== undefined && props.FMax !== undefined && <div className="col-6 p-0">
                 <i className="fas fa-skull-crossbones mr-1"></i>
-                {props.FMin + " - " + props.FMax}
-            </div>
-            <div className="col-6 p-0">
+                {`${props.FMin} - ${props.FMax}`}
+            </div>}
+            {props.tAvg !== undefined && <div className="col-6 p-0">
                 <i className="far fa-clock mr-1"></i>
-                {props.tAvg + props.locale + " (" + props.tMin + props.locale + " - " + props.tMax + props.locale + ")"}
-            </div>
-            <div className="col-6 p-0">
+                {`${props.tAvg}${pveStrings.s}`}
+                {props.tMin !== undefined && props.tMax !== undefined && ` (${props.tMin}${pveStrings.s} - ${props.tMax}${pveStrings.s})`}
+            </div>}
+            {props.ttwAvg && <div className="col-6 p-0">
                 <i className="far fa-hourglass mr-1"></i>
-                {props.ttwAvg + props.locale}
-            </div>
+                {`${props.ttwAvg}${pveStrings.s}`}
+            </div>}
 
         </div>
     )
