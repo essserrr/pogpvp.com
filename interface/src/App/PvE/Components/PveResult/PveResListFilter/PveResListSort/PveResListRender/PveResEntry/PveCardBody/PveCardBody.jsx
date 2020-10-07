80b0b12d6b7ef242af2f222a71fa7f1158409dd9
Essserrr <blizzz93@gmail.com>
2020-10-07 21:26:04 +0900

    stats calculation added

--- interface/src/App/PvE/Components/PveResult/PveResListFilter/PveResListSort/PveResListRender/PveResEntry/PveCardBody/PveCardBody.jsx
+++ interface/src/App/PvE/Components/PveResult/PveResListFilter/PveResListSort/PveResListRender/PveResEntry/PveCardBody/PveCardBody.jsx
@@ -20,13 +20,45 @@ class PveCardBody extends React.PureComponent {
         pveStrings.setLanguage(getCookie("appLang") ? getCookie("appLang") : "en")
     }
 
-    damageString(damage) {
+    cropDamage(damage) {
         return this.props.tables.hp[this.props.snapshot.bossObj.Tier] < damage ?
             this.props.tables.hp[this.props.snapshot.bossObj.Tier] : damage
     }
 
+    processStats(avgStats) {
+        let dAvg = (avgStats.DAvg / (this.props.tables.hp[this.props.snapshot.bossObj.Tier]) * 100).toFixed(1)
+        let dMin = (avgStats.DMin / (this.props.tables.hp[this.props.snapshot.bossObj.Tier]) * 100).toFixed(1)
+        let dMax = (avgStats.DMax / (this.props.tables.hp[this.props.snapshot.bossObj.Tier]) * 100).toFixed(1)
+
+        let tAvg = (avgStats.TAvg / 1000).toFixed(0)
+
+        return {
+            dAvg: dAvg,
+            dMin: dMin,
+            dMax: dMax,
+
+            tAvg: (avgStats.TAvg / 1000).toFixed(0),
+            tMin: (avgStats.TMin / 1000).toFixed(0),
+            tMax: (avgStats.TMax / 1000).toFixed(0),
+
+            dpsAvg: (avgStats.DAvg / (this.props.tables.timer[this.props.snapshot.bossObj.Tier] - tAvg)).toFixed(1),
+            dpsMin: (avgStats.DMin / (this.props.tables.timer[this.props.snapshot.bossObj.Tier] - tAvg)).toFixed(1),
+            dpsMax: (avgStats.DMax / (this.props.tables.timer[this.props.snapshot.bossObj.Tier] - tAvg)).toFixed(1),
+
+            plAvg: (100 / dAvg).toFixed(2),
+            plMin: Math.ceil(100 / dMax),
+            plMax: Math.ceil(100 / dMin),
+
+            ttwAvg: Math.ceil((this.props.tables.timer[this.props.snapshot.bossObj.Tier] - tAvg) * 100 / dAvg),
+
+            FMin: avgStats.FMin,
+            FMax: avgStats.FMax,
+        }
+    }
+
     render() {
         return (
+
             <div className="pvecard-body col-12 m-0 p-0 p-2 my-1 " key={this.props.stats.BQ + this.props.stats.BCh}>
                 <div className="col-12 d-flex align-items-center m-0 p-0">
                     <WeatherMoves
@@ -37,30 +69,26 @@ class PveCardBody extends React.PureComponent {
                 </div>
                 <div className="col-12 m-0 p-0 mt-2">
                     <HpBar
-                        upbound={((this.props.tables.hp[this.props.snapshot.bossObj.Tier] -
-                            this.damageString(this.props.stats.DMin)) / (this.props.tables.hp[this.props.snapshot.bossObj.Tier]) * 100).toFixed(1)}
-                        lowbound={((this.props.tables.hp[this.props.snapshot.bossObj.Tier] -
-                            this.damageString(this.props.stats.DMax)) / (this.props.tables.hp[this.props.snapshot.bossObj.Tier]) * 100).toFixed(1)}
-                        length={((this.props.tables.hp[this.props.snapshot.bossObj.Tier] -
-                            this.damageString(this.props.stats.DAvg)) / (this.props.tables.hp[this.props.snapshot.bossObj.Tier]) * 100).toFixed(1)}
+
+                        up={((this.props.tables.hp[this.props.snapshot.bossObj.Tier] - this.cropDamage(this.props.stats.DMin)) /
+                            (this.props.tables.hp[this.props.snapshot.bossObj.Tier]) * 100).toFixed(1)}
+                        low={((this.props.tables.hp[this.props.snapshot.bossObj.Tier] - this.cropDamage(this.props.stats.DMax)) /
+                            (this.props.tables.hp[this.props.snapshot.bossObj.Tier]) * 100).toFixed(1)}
+                        avg={((this.props.tables.hp[this.props.snapshot.bossObj.Tier] - this.cropDamage(this.props.stats.DAvg)) /
+                            (this.props.tables.hp[this.props.snapshot.bossObj.Tier]) * 100).toFixed(1)}
                     />
                 </div>
-                <div className="pvecard-body--font col-12 m-0 p-0">
+                <div className="col-12 m-0 p-0">
                     <HpRemaining
-                        locale={pveStrings.hprem}
-                        DAvg={this.damageString(this.props.stats.DAvg)}
-                        DMax={this.damageString(this.props.stats.DMax)}
-                        DMin={this.damageString(this.props.stats.DMin)}
-                        NOfWins={this.props.stats.NOfWins.toFixed(1)}
-                        tierHP={this.props.tables.hp[this.props.snapshot.bossObj.Tier]}
+                        avg={this.props.tables.hp[this.props.snapshot.bossObj.Tier] - this.cropDamage(this.props.stats.DAvg)}
+                        max={this.props.tables.hp[this.props.snapshot.bossObj.Tier] - this.cropDamage(this.props.stats.DMax)}
+                        min={this.props.tables.hp[this.props.snapshot.bossObj.Tier] - this.cropDamage(this.props.stats.DMin)}
+                        nbOfWins={this.props.stats.NOfWins.toFixed(1)}
                     />
                 </div>
                 <div className="col-12 m-0 p-0">
                     <FightStats
-                        locale={pveStrings.s}
-                        tables={this.props.tables}
-                        snapshot={this.props.snapshot}
-                        avgStats={this.props.stats}
+                        {...this.processStats(this.props.stats)}
                     />
                 </div>
             </div>
