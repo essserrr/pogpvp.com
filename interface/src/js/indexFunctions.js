193d6ebfb637855749037492a648df85f44bf9b9
Essserrr <blizzz93@gmail.com>
2020-10-10 17:42:15 +0900

    level processing changed

--- interface/src/js/indexFunctions.js
+++ interface/src/js/indexFunctions.js
@@ -279,13 +279,13 @@ export function checkIV(IV) {
 export function checkLvl(lvl) {
     let lvlNumber = Number(lvl)
     if (isNaN(lvlNumber)) {
-        return 1
+        return 0
     }
     if (lvlNumber > 45) {
         return 45
     }
-    if (lvlNumber < 1) {
-        return 1
+    if (lvlNumber < 0) {
+        return 0
     }
     if (!Number.isInteger(lvlNumber / 0.5)) {
         return Math.trunc(lvlNumber)
