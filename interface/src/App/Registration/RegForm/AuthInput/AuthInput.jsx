cd10217bb1732285062eec3d2b1a9bb5f5e41f3e
Essserrr <blizzz93@gmail.com>
2020-10-24 20:59:33 +0900

    auth input is a wrapper above text field now

--- interface/src/App/Registration/RegForm/AuthInput/AuthInput.jsx
+++ interface/src/App/Registration/RegForm/AuthInput/AuthInput.jsx
@@ -1,27 +1,55 @@
-import React from "react"
-import "./AuthInput.scss"
+import React from "react";
+import PropTypes from 'prop-types';
+
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+    authInput: {
+        width: "100%",
+    },
+    helperText: {
+        "&:not(.Mui-error)": {
+            visibility: "hidden",
+        }
+    }
+}));
 
 const AuthInput = React.memo(function (props) {
+    const { label, type, name, errorText, onChange, ...other } = props;
+    const classes = useStyles();
+
     return (
-        <div className="col-12 px-0">
-            <div className="col-12 px-0 auth-input__text text-left">
-                {props.labelLeft}
-            </div>
-            <input
-                className={"auth-input form-control mx-0 auth-input--mt " + (props.notOk !== "" ? "auth-input--alert" : "")}
+        <TextField
+            label={label}
+            type={type}
+            name={name}
+            helperText={errorText !== "" ? errorText : "default"}
+            error={errorText !== ""}
+            onChange={onChange}
 
-                name={props.name}
-                value={props.value}
+            className={classes.authInput}
+            FormHelperTextProps={{
+                className: classes.helperText,
+            }}
 
-                placeholder={props.place}
-                type={props.type}
-                autoComplete={props.aCompleteOff ? "new-password" : ""}
-
-                onChange={props.onChange}
-            />
-            {props.notOk !== "" && <div className="col-12 px-0 auth-input__alert-text text-left">{props.notOk}</div>}
-        </div>
+            {...other}
+        />
     )
 })
 
-export default AuthInput
+export default AuthInput;
+
+AuthInput.propTypes = {
+    label: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.node,
+    ]),
+    type: PropTypes.string,
+    name: PropTypes.string,
+    errorText: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.node,
+    ]),
+    onChange: PropTypes.func,
+};
