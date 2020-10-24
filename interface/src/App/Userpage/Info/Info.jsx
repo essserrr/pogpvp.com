bedea608e9a3272f335475a7685ba49c1d43385b
Essserrr <blizzz93@gmail.com>
2020-10-24 13:06:16 +0900

    content title added

--- interface/src/App/Userpage/Info/Info.jsx
+++ interface/src/App/Userpage/Info/Info.jsx
@@ -1,16 +1,21 @@
-import React from "react"
-import { getCookie } from "../../../js/getCookie"
-import { connect } from "react-redux"
+import React from "react";
+import { getCookie } from "../../../js/getCookie";
+import { connect } from "react-redux";
 
-import SiteHelm from "../../SiteHelm/SiteHelm"
-import TimeConverter from "./TimeConverter"
-import Loader from "../../PvpRating/Loader"
-import Errors from "../../PvP/components/Errors/Errors"
-import { refresh } from "../../../AppStore/Actions/refresh"
-import LocalizedStrings from "react-localization"
-import { userLocale } from "../../../locale/userLocale"
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
 
-import "./Info.scss"
+import SiteHelm from "../../SiteHelm/SiteHelm";
+import TimeConverter from "./TimeConverter";
+import ContentTitle from "../ContentTitle/ContentTitle"
+
+
+import Errors from "../../PvP/components/Errors/Errors";
+import { refresh } from "../../../AppStore/Actions/refresh";
+import LocalizedStrings from "react-localization";
+import { userLocale } from "../../../locale/userLocale";
+
+import "./Info.scss";
 
 let strings = new LocalizedStrings(userLocale)
 
@@ -59,42 +64,39 @@ class Info extends React.PureComponent {
 
     render() {
         return (
-            <div className="col p-3 text-center">
+            <Grid container justify="center">
                 <SiteHelm
                     url="https://pogpvp.com/profile/info"
                     header={strings.pageheaders.usrinfo}
                     descr={strings.pagedescriptions.usr}
                     noindex={true}
                 />
-                {this.state.loading &&
-                    <Loader
-                        color="black"
-                        weight="500"
-                        locale={strings.loading}
-                        loading={this.state.loading}
-                    />}
+
+                {this.state.loading && <LinearProgress color="secondary" />}
+
                 {this.state.error !== "" && <Errors class="alert alert-danger p-2" value={this.state.error} />}
-                {this.state.error === "" && this.state.uInfo.Username &&
-                    <div className="row mx-0 justify-content-center">
-                        <div className="col-12 col-md-8 col-lg-6 px-0">
-                            <div className="user-info__title col-12 px-0 mb-3">
+
+                {this.state.error === "" && !this.state.loading && this.state.uInfo.Username &&
+                    <Grid item sm={12} md={8} lg={6}>
+                        <Grid item xs={12}>
+                            <ContentTitle>
                                 {strings.info.title}
-                            </div>
-                            <div className="col px-0 py-2 user-info--border user-info__goverable-col">
-                                <span className={"user-info__text"}>{strings.info.name + ": "}</span>
-                                <span className={"font-weight-bold"}>{this.state.uInfo.Username}</span>
-                            </div>
-                            <div className="col px-0 py-2 user-info--border user-info__goverable-col">
-                                <span className={"user-info__text"}>{strings.info.email + ": "}</span>
-                                <span className={"font-weight-bold"}>{this.state.uInfo.Email}</span>
-                            </div>
-                            <div className="col px-0 py-2 user-info__goverable-col">
-                                <span className={"user-info__text"}>{strings.info.reg + ": "}</span>
-                                <span className={"font-weight-bold"}><TimeConverter time={this.state.uInfo.RegAt} getTime={false} /></span>
-                            </div>
+                            </ContentTitle>
+                        </Grid>
+                        <div className="col px-0 py-2 user-info--border user-info__goverable-col">
+                            <span className={"user-info__text"}>{strings.info.name + ": "}</span>
+                            <span className={"font-weight-bold"}>{this.state.uInfo.Username}</span>
                         </div>
-                    </div>}
-            </div>
+                        <div className="col px-0 py-2 user-info--border user-info__goverable-col">
+                            <span className={"user-info__text"}>{strings.info.email + ": "}</span>
+                            <span className={"font-weight-bold"}>{this.state.uInfo.Email}</span>
+                        </div>
+                        <div className="col px-0 py-2 user-info__goverable-col">
+                            <span className={"user-info__text"}>{strings.info.reg + ": "}</span>
+                            <span className={"font-weight-bold"}><TimeConverter timestamp={this.state.uInfo.RegAt} getHours={false} /></span>
+                        </div>
+                    </Grid>}
+            </Grid>
         )
     }
 }
