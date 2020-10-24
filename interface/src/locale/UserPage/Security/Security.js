74276c933607d936e448ed036afb33c702844193
Essserrr <blizzz93@gmail.com>
2020-10-24 18:53:05 +0900

    security locale divided

---
+++ interface/src/locale/UserPage/Security/Security.js
@@ -0,0 +1,43 @@
+export const userLocale = {
+    en: {
+        pageheaders: {
+            usrsec: "User page: Security  |  PogPvP.com",
+        },
+        pagedescriptions: {
+            usr: "User page",
+        },
+        security: {
+            os: "OS",
+            ip: "IP",
+            br: "Browser",
+            acts: "Active sessions",
+            soutall: "Sign out from all sessions",
+            chpass: "Change password",
+            npass: "New password",
+            ok: "Password has been successfully changed",
+            oldpass: "Old password",
+            confnpass: "Confirm new password",
+        },
+    },
+
+    ru: {
+        pageheaders: {
+            usrsec: "Страница пользователя: Безопасность  |  PogPvP.com",
+        },
+        pagedescriptions: {
+            usr: "Страница пользователя",
+        },
+        security: {
+            os: "ОС",
+            ip: "IP",
+            br: "Браузер",
+            acts: "Активные сессии",
+            soutall: "Завершить все сессии",
+            chpass: "Изменить пароль",
+            npass: "Новый пароль",
+            ok: "Пароль успешно изменен",
+            oldpass: "Старый пароль",
+            confnpass: "Подтвердите новый пароль",
+        },
+    }
+}
