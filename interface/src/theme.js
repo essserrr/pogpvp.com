5a484aa2575378e8ae56a248e37ff41aa8ce757e
Essserrr <blizzz93@gmail.com>
2020-10-24 11:02:20 +0900

    theme provider added

---
+++ interface/src/theme.js
@@ -0,0 +1,24 @@
+import { red } from '@material-ui/core/colors';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+// A custom theme for this app
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      text: "#1a1a1a",
+      main: '#556cd6',
+      background: "#eff0f1",
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+});
+
+export default theme;
