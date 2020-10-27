ecd862e8c52d5635917aa6ddaecdafdb0ed8746f
Essserrr <blizzz93@gmail.com>
2020-10-27 22:34:52 +0900

    material ui based serchable select impelmnted

---
+++ interface/src/App/Components/SerachableSelect/SerachableSelect.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+
+const useStyles = makeStyles((theme) => ({
+    serachableSelect: {
+        width: "100%",
+    },
+}));
+
+const SerachableSelect = React.memo(function SerachableSelect(props) {
+    const { helperText, children, label, type, name, category, attr, onChange, ...other } = props;
+    const classes = useStyles();
+
+    return (
+        <Autocomplete
+            {...other}
+            className={classes.serachableSelect}
+            options={props.children}
+            getOptionLabel={(option) => option.title}
+
+
+            onChange={(event, selectOption) => onChange(event, { type: type, name: name, category: category, attr: attr, ...selectOption })}
+
+
+            renderInput={(params) => <TextField {...params} label={label} helperText={helperText} />}
+        />
+    )
+});
+
+export default SerachableSelect;
+
+SerachableSelect.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.object),
+    ]),
+    label: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.node,
+    ]),
+    type: PropTypes.string,
+    name: PropTypes.string,
+    category: PropTypes.string,
+    attr: PropTypes.string,
+
+    helperText: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.node,
+    ]),
+    onChange: PropTypes.func,
+    className: PropTypes.string,
+};
