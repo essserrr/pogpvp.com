3f4d5cd3ee45fdb6531ba627e2dad159178ed0dc
Essserrr <blizzz93@gmail.com>
2020-10-28 22:15:47 +0900

    new portion of bugfixes

--- interface/src/js/movePoolBuilder.js
+++ interface/src/js/movePoolBuilder.js
@@ -1,14 +1,14 @@
 export class MovePoolBuilder {
     constructor() {
-      this.pokemon = {};
-      //generated 
-      this.quickMovePool = [];
-      this.chargeMovePool = [];
+        this.pokemon = {};
+        //generated 
+        this.quickMovePool = [];
+        this.chargeMovePool = [];
     }
-  
+
     createMovePool(name, pokTable, locale, isBoss, additionalQ, addtionalCh) {
         if (!pokTable || !name || !pokTable[name]) return;
-        this.pokemon = {...pokTable[name]};
+        this.pokemon = { ...pokTable[name] };
         this.filterMoves(isBoss);
 
         this.quickMovePool = this.returnMovePool(locale, this.pokemon.QuickMoves, additionalQ);
@@ -16,27 +16,27 @@ export class MovePoolBuilder {
     }
 
     filterMoves(isBoss) {
-        this.pokemon.QuickMoves = this.pokemon.QuickMoves.filter(elem => (isBoss && !!pokemon.EliteMoves[elem]) ? false : elem !== "");
-        this.pokemon.ChargeMoves = this.pokemon.ChargeMoves.filter(elem => (isBoss && (!!pokemon.EliteMoves[elem] || elem === "Return")) ? false : elem !== "");
+        this.pokemon.QuickMoves = this.pokemon.QuickMoves.filter(elem => (isBoss && !!this.pokemon.EliteMoves[elem]) ? false : elem !== "");
+        this.pokemon.ChargeMoves = this.pokemon.ChargeMoves.filter(elem => (isBoss && (!!this.pokemon.EliteMoves[elem] || elem === "Return")) ? false : elem !== "");
     }
 
     returnMovePool(locale, moves, additionalMoves) {
-        let movePool = [{value: "",  title: locale.none}];
+        let movePool = [{ value: "", title: locale.none }];
         moves.forEach(moveName => {
-            movePool.push({value: moveName,  title: `${moveName}${!!this.pokemon.EliteMoves[moveName] ? "*" : ""}`});
+            movePool.push({ value: moveName, title: `${moveName}${!!this.pokemon.EliteMoves[moveName] ? "*" : ""}` });
         });
         this.pushAdditional(additionalMoves, moves, movePool);
-        movePool.push({value: "Select...",  title: locale.select});
+        movePool.push({ value: "Select...", title: locale.select });
         return movePool;
     }
-  
+
     pushAdditional(additionalMoves, moves, movePool) {
         if (!additionalMoves || !moves || !movePool) return;
         //iterate over additional moves
         additionalMoves.forEach((item, i) => {
             //if additional move is invalid somehow  - skip
             if (!item) return;
-            if (!moves.includes(item)) movePool.push({value: item,  title: `${item}*`});
+            if (!moves.includes(item)) movePool.push({ value: item, title: `${item}*` });
         });
     }
-  }
+}
