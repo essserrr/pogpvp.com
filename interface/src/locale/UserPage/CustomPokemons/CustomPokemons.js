5b1c52a5900c79c407d5fb19adeccd9f40fb442e
Essserrr <blizzz93@gmail.com>
2020-10-31 13:02:44 +0900

    custom pokemon locale separated

---
+++ interface/src/locale/UserPage/CustomPokemons/CustomPokemons.js
@@ -0,0 +1,120 @@
+export const userLocale = {
+    en: {
+        impExp: "Import/Export",
+
+        userpok: {
+            changes: "Save changes",
+            poktitle: "User pokemon",
+            grouptitle: "User groups",
+
+            addpok: "Add pokemon",
+            or: "OR",
+            filt: "Filters",
+
+            pokname: "Pokemon name",
+            qname: "Quick move",
+            chname: "Charge move",
+
+            parties: "Your groups",
+            selectpok: "Select pokemon",
+            actparty: "Pokemon in group:",
+            partyname: "Enter group name",
+
+            savegroup: "Add group",
+            deletegroup: "Delete group",
+
+            err: {
+                errname: "You must select a pokemon",
+                errq: "You must select a quick move",
+                errch: "You must select a charge move",
+            },
+        },
+
+        err: {
+            token: "You must check the captcha",
+            ness: "Necessary field",
+            match: "Passwords don't match",
+            symb: " contains prohibited symbols",
+            symb2: " contain prohibited symbols",
+
+            emailf: "Incorrect Email format",
+            lesseq: {
+                l1: " must be less than or equal ",
+                l2: " must be less than or equal ",
+                l3: " must be less than or equal ",
+                c: " characters",
+            },
+            longer: {
+                l1: " must be longer than ",
+                l2: " must be longer than ",
+                l3: " must be longer than ",
+            },
+        },
+        pageheaders: {
+            usrpok: "User page: User pokemon  |  PogPvP.com",
+        },
+        pagedescriptions: {
+            reg: "User sign up form",
+        },
+    },
+
+    ru: {
+        impExp: "Импорт/Экспорт",
+
+        userpok: {
+            changes: "Сохранить изменения",
+            poktitle: "Покемоны пользователя",
+            grouptitle: "Группы пользователя",
+
+            addpok: "Добавить покемона",
+            or: "ИЛИ",
+            filt: "Фильтры",
+
+            pokname: "Имя покемона",
+            qname: "Быстрое умение",
+            chname: "Заряжаемое умение",
+
+            parties: "Ваши группы",
+            selectpok: "Выберите покемона",
+            actparty: "Покемонов в группе:",
+            partyname: "Введите имя группы",
+
+            savegroup: "Добавить группу",
+            deletegroup: "Удалить группу",
+
+            err: {
+                errname: "Вы должны выбрать покемона",
+                errq: "Вы должны выбрать быстрое умение",
+                errch: "Вы должны выбрать заряжаемое умение",
+            },
+        },
+
+        err: {
+            token: "Вы должны пройти проверку",
+            ness: "Обязательное поле",
+            match: "Пароли не совпадают",
+            symb: " содержит запрещенные символы",
+            symb2: " содержат запрещенные символы",
+
+            emailf: "Неверный формат адреса электронной почты",
+            lesseq: {
+                l1: " должно быть меньше либо равно ",
+                l2: " должен быть меньше либо равен ",
+                l3: " должны быть меньше либо равны ",
+                c: " символов",
+            },
+            longer: {
+                l1: " должно быть длиннее, чем ",
+                l2: " должен быть длиннее, чем ",
+                l3: " должны быть длиннее, чем ",
+            },
+        },
+
+        pageheaders: {
+            usrpok: "Страница пользователя: Покемоны пользователя  |  PogPvP.com",
+        },
+        pagedescriptions: {
+            usr: "Страница пользователя",
+        },
+    }
+}
