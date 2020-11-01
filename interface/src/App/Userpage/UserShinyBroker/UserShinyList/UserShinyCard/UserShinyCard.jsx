43c4bdcb71a853bd87415c1a8a4025c863d808df
Essserrr <blizzz93@gmail.com>
2020-11-01 20:24:51 +0900

    Shiny Card reworked

--- interface/src/App/Userpage/UserShinyBroker/UserShinyList/UserShinyCard/UserShinyCard.jsx
+++ interface/src/App/Userpage/UserShinyBroker/UserShinyList/UserShinyCard/UserShinyCard.jsx
@@ -1,35 +1,83 @@
-import React from "react"
-import ReactTooltip from "react-tooltip"
+import React from "react";
+import PropTypes from 'prop-types';
 
-import PokemonIconer from "../../../../PvP/components/PokemonIconer/PokemonIconer"
-import CloseButton from "../../../../PvP/components/MagicBox/CloseButton"
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Tooltip from '@material-ui/core/Tooltip';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
-import { ReactComponent as Shiny } from "../../../../../icons/shiny.svg"
+import Iconer from "App/Components/Iconer/Iconer";
+import CloseButton from "App/Components/CloseButton/CloseButton";
 
-import "./UserShinyCard.scss"
+import { ReactComponent as Shiny } from "icons/shiny.svg"
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        margin: `${theme.spacing(0.5)}px`,
+
+        borderRadius: "3px",
+        border: `1px solid ${theme.palette.text.primary}`,
+        fontWeight: "bold",
+    },
+    uShCard: {
+        padding: `${theme.spacing(0.5)}px`,
+    },
+    uShCardContainer: {
+        position: "relative",
+    },
+
+    uShCardShiny: {
+        width: "18px",
+        height: "18px",
+
+        position: "absolute",
+        right: "-3px",
+    },
+}));
 
 const UserShinyCard = React.memo(function (props) {
-    return (
-        <div className="ushinycard col-auto my-1 mx-1 d-flex align-items-center">
-            {props.value.Amount && `${props.value.Amount}X`}
-            <ReactTooltip
-                className={"infoTip"}
-                id={props.attr + props.value.Name} effect="solid"
-                place={"top"}
-                multiline={true} >
-                {props.value.Name}
-            </ReactTooltip>
-            <div data-tip data-for={props.attr + props.value.Name} className="ushinycard__container col-auto p-0">
-                {props.pokemonTable[props.value.Name] && <PokemonIconer
-                    src={props.pokemonTable[props.value.Name].Number +
-                        (props.pokemonTable[props.value.Name].Forme !== "" ? "-" + props.pokemonTable[props.value.Name].Forme : "")}
-                    class={"ushinycard__pok mr-1"}
-                />}
-                {props.value.Type === "Shiny" ? <Shiny className="ushinycard__shiny" /> : null}
-            </div>
-            {props.onClick && <CloseButton attr={props.attr} index={props.value.Name} onClick={props.onClick} />}
-        </div>
-    )
-})
+    const classes = useStyles();
 
-export default UserShinyCard
+    function onClickWrapper(event) {
+        if (props.onClick) {
+            event.stopPropagation()
+            props.onClick({ attr: props.attr, index: props.index })
+        }
+    }
+
+    const pokemon = props.pokemonTable[props.value.Name]
+    return (
+        <Paper elevation={4} className={classes.paper}>
+            <Grid container alignItems="center" className={classes.uShCard}>
+                {props.value.Amount && `${props.value.Amount}X`}
+                <Tooltip title={<Typography color="inherit">{props.value.Name}</Typography>}>
+                    <Grid xs="auto" className={classes.uShCardContainer}>
+                        {pokemon &&
+                            <Iconer
+                                size={36}
+                                fileName={`${pokemon.Number}${pokemon.Forme !== "" ? `-${pokemon.Forme}` : ""}`}
+                                folderName="/pokemons/"
+                            />}
+                        {props.value.Type === "Shiny" ? <Shiny className={classes.uShCardShiny} /> : null}
+                    </Grid>
+                </Tooltip>
+                {props.onClick && <CloseButton onClick={onClickWrapper} />}
+            </Grid>
+        </Paper>
+    )
+});
+
+export default UserShinyCard;
+
+UserShinyCard.propTypes = {
+    attr: PropTypes.string,
+    index: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number,
+    ]),
+    onClick: PropTypes.func,
+
+    pokemonTable: PropTypes.object,
+    value: PropTypes.object,
+};
