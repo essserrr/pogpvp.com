ce781d32c0c0c601603fe7887574961977191983
Essserrr <blizzz93@gmail.com>
2020-11-01 11:32:40 +0900

    stat tips added

--- interface/src/App/PvP/components/Stats/Stats.jsx
+++ interface/src/App/PvP/components/Stats/Stats.jsx
@@ -1,81 +1,75 @@
-import React from "react"
+import React from "react";
+import LocalizedStrings from "react-localization";
 
-import ReactTooltip from "react-tooltip"
+import Tooltip from '@material-ui/core/Tooltip';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
-import LocalizedStrings from "react-localization"
-import { locale } from "../../../../locale/locale"
-import { getCookie } from "../../../../js/getCookie"
+import { locale } from "locale/locale"
+import { getCookie } from "js/getCookie"
 
 import Grid from '@material-ui/core/Grid';
 import Input from 'App/Components/Input/Input';
-
-import "./Stats.scss"
 
 let strings = new LocalizedStrings(locale)
 
 const Stats = React.memo(function (props) {
     strings.setLanguage(getCookie("appLang") ? getCookie("appLang") : "en")
     return (
-        <>
-            {/*<ReactTooltip
-                className={"infoTip"}
-                id={props.attr + "inlvl"} effect="solid">
-                {strings.stats.lvl + ": 1-45"}
-            </ReactTooltip>
-            <ReactTooltip
-                className={"infoTip"}
-                id={props.attr + "inatk"} effect="solid">
-                {strings.effStats.atk + " IV: 0-15"}
-            </ReactTooltip>
-            <ReactTooltip
-                className={"infoTip"}
-                id={props.attr + "indef"} effect="solid">
-                {strings.effStats.def + " IV: 0-15"}
-            </ReactTooltip>
-            <ReactTooltip
-                className={"infoTip"}
-                id={props.attr + "insta"} effect="solid">
-                {strings.effStats.sta + " IV: 0-15"}
-            </ReactTooltip>*/}
-            <Grid container >
-                <Grid item xs >
-                    <Input
-                        label={strings.stats.lvl}
-                        name="Lvl"
-                        attr={props.attr}
-                        value={props.Lvl}
-                        onChange={props.onChange}
-                    />
-                </Grid>
-                <Grid item xs >
-                    <Input
-                        name="Atk"
-                        attr={props.attr}
-                        value={props.Atk}
-                        onChange={props.onChange}
-                        label={strings.effStats.atk}
-                    />
-                </Grid>
-                <Grid item xs >
-                    <Input
-                        name="Def"
-                        attr={props.attr}
-                        value={props.Def}
-                        onChange={props.onChange}
-                        label={strings.effStats.def}
-                    />
-                </Grid>
-                <Grid item xs >
-                    <Input
-                        name="Sta"
-                        attr={props.attr}
-                        value={props.Sta}
-                        onChange={props.onChange}
-                        label={strings.effStats.sta}
-                    />
-                </Grid>
+        <Grid container >
+            <Grid item xs >
+                <Tooltip title={<Typography color="inherit">{`${strings.stats.lvl}: 1-45`}</Typography>}>
+                    <Box>
+                        <Input
+                            label={strings.stats.lvl}
+                            name="Lvl"
+                            attr={props.attr}
+                            value={props.Lvl}
+                            onChange={props.onChange}
+                        />
+                    </Box>
+                </Tooltip>
             </Grid>
-        </>
+            <Grid item xs >
+                <Tooltip title={<Typography color="inherit">{`${strings.effStats.atk} IV: 0-15`}</Typography>}>
+                    <Box>
+                        <Input
+                            name="Atk"
+                            attr={props.attr}
+                            value={props.Atk}
+                            onChange={props.onChange}
+                            label={strings.effStats.atk}
+                        />
+                    </Box>
+                </Tooltip>
+            </Grid>
+            <Grid item xs >
+                <Tooltip title={<Typography color="inherit">{`${strings.effStats.def} IV: 0-15`}</Typography>}>
+                    <Box>
+                        <Input
+                            name="Def"
+                            attr={props.attr}
+                            value={props.Def}
+                            onChange={props.onChange}
+                            label={strings.effStats.def}
+                        />
+                    </Box>
+                </Tooltip>
+            </Grid>
+            <Grid item xs >
+                <Tooltip title={<Typography color="inherit">{`${strings.effStats.sta} IV: 0-15`}</Typography>}>
+                    <Box>
+                        <Input
+                            name="Sta"
+                            attr={props.attr}
+                            value={props.Sta}
+                            onChange={props.onChange}
+                            label={strings.effStats.sta}
+                        />
+                    </Box>
+                </Tooltip>
+            </Grid>
+        </Grid>
     )
 
 });
