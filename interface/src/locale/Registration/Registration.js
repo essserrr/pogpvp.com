4bc19a3494a47783fdd859b66deb80254aed9c5b
Essserrr <blizzz93@gmail.com>
2020-11-04 21:29:22 +0900

    registration reworked

---
+++ interface/src/locale/Registration/Registration.js
@@ -0,0 +1,105 @@
+export const userLocale = {
+    en: {
+        propc: "By signing up you agree with ",
+        and: "and",
+        pol: {
+            t: "Terms and Conditions",
+            p: "Privacy Policy",
+        },
+        signup: {
+            newlin: "Have an account?",
+            reg: "Sign up",
+            toreg: "Sign up",
+            uname: "Username",
+            pass: "Password",
+            cpass: "Confirm password",
+            email: "Email",
+            forg: "Forgot your password?",
+            rest: "Restore",
+            tolog: "Log in",
+        },
+
+
+        err: {
+            token: "You must check the captcha",
+            ness: "Necessary field",
+            match: "Passwords don't match",
+            symb: " contains prohibited symbols",
+            symb2: " contain prohibited symbols",
+
+            emailf: "Incorrect Email format",
+            lesseq: {
+                l1: " must be less than or equal ",
+                l2: " must be less than or equal ",
+                l3: " must be less than or equal ",
+                c: " characters",
+            },
+            longer: {
+                l1: " must be longer than ",
+                l2: " must be longer than ",
+                l3: " must be longer than ",
+            },
+        },
+
+
+
+        pageheaders: {
+            reg: "User sign up form  |  PogPvP.com",
+        },
+        pagedescriptions: {
+            reg: "User sign up form",
+        },
+    },
+
+    ru: {
+        loading: "Загрузка...",
+        propc: "Регистрируясь вы соглашаетесь с ",
+        and: "и",
+        pol: {
+            t: "Условия и положения",
+            p: "Политика конфиденциальности",
+        },
+        signup: {
+            newlin: "Есть аккаунт?",
+            reg: "Регистрация",
+            toreg: "Зарегистрироваться",
+            uname: "Имя пользователя",
+            pass: "Пароль",
+            cpass: "Подтвердите пароль",
+            email: "Адрес электронной почты",
+            forg: "Забыли ваш пароль?",
+            rest: "Восстановить",
+            tolog: "Войти",
+        },
+
+
+        err: {
+            token: "Вы должны пройти проверку",
+            ness: "Обязательное поле",
+            match: "Пароли не совпадают",
+            symb: " содержит запрещенные символы",
+            symb2: " содержат запрещенные символы",
+
+            emailf: "Неверный формат адреса электронной почты",
+            lesseq: {
+                l1: " должно быть меньше либо равно ",
+                l2: " должен быть меньше либо равен ",
+                l3: " должны быть меньше либо равны ",
+                c: " символов",
+            },
+            longer: {
+                l1: " должно быть длиннее, чем ",
+                l2: " должен быть длиннее, чем ",
+                l3: " должны быть длиннее, чем ",
+            },
+        },
+
+
+        pageheaders: {
+            reg: "Форма регистрации пользователя  |  PogPvP.com",
+        },
+        pagedescriptions: {
+            reg: "Форма регистрации пользователя",
+        },
+    }
+}
