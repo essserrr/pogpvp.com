0337628f2fabdccca36872198c02f16ec84d27a7
Essserrr <blizzz93@gmail.com>
2020-11-05 17:25:26 +0900

    NavigationBlock Reworked

--- interface/src/App/Pokedex/PokeCard/NavigationBlock/NavigationBlock.jsx
+++ interface/src/App/Pokedex/PokeCard/NavigationBlock/NavigationBlock.jsx
@@ -1,50 +1,48 @@
-import React from "react"
-import ReactTooltip from "react-tooltip"
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from 'prop-types';
+
+import Grid from '@material-ui/core/Grid';
+import DoubleArrowIcon from '@material-ui/icons/DoubleArrow';
+
+import Typography from '@material-ui/core/Typography';
+import Tooltip from '@material-ui/core/Tooltip';
 
 const NavigationBlock = React.memo(function (props) {
+
     function buttonsConfig() {
-        if (props.prev && props.next) {
-            return "justify-content-between"
-        }
-        if (props.prev) {
-            return "justify-content-start"
-        }
-        return "justify-content-end"
+        if (props.prev && props.next) { return "space-between" }
+        if (props.prev) { return "flex-start" }
+        return "flex-end"
     }
 
     return (
-        <div className={(props.class ? props.class : "row m-0 mb-2 ") + buttonsConfig()}>
-            {props.prev && <Link to={navigator.userAgent === "ReactSnap" ? "/" : props.prev}>
-                <i
-                    attr={"prev"}
-                    className="fas fa-angle-double-left fa-2x clickable"
-                    data-tip data-for={props.prevTitle ? "prev" : ""}
-                ></i>
-                <ReactTooltip
-                    className={"infoTip"}
-                    multiline={true}
-                    id={props.prevTitle ? "prev" : ""} effect="solid">
-                    {props.prevTitle}
-                </ReactTooltip>
-            </Link>}
-            {props.next && <Link to={navigator.userAgent === "ReactSnap" ? "/" : props.next}>
-                <i
-                    attr={"next"}
-                    className="fas fa-angle-double-right fa-2x clickable"
-                    data-tip data-for={props.nextTitle ? "next" : ""}
-                ></i>
-                <ReactTooltip
-                    className={"infoTip"}
-                    multiline={true}
-                    id={props.nextTitle ? "next" : ""} effect="solid">
-                    {props.nextTitle}
-                </ReactTooltip>
+        <Grid container justify={buttonsConfig()}>
+            {props.prev &&
+                <Link to={navigator.userAgent === "ReactSnap" ? "/" : props.prev}>
+                    <Tooltip placement="top" arrow
+                        title={<Typography color="inherit">{props.prevTitle}</Typography>}>
+                        <DoubleArrowIcon style={{ transform: "rotate(180deg)", fontSize: "32px" }} />
+                    </Tooltip>
+                </Link>}
 
-            </Link >}
-        </div >
+            {props.next &&
+                <Link to={navigator.userAgent === "ReactSnap" ? "/" : props.next}>
+                    <Tooltip placement="top" arrow
+                        title={<Typography color="inherit">{props.nextTitle}</Typography>}>
+                        <DoubleArrowIcon style={{ fontSize: "32px" }} />
+                    </Tooltip>
+                </Link >}
+        </Grid>
     )
 });
 
-
 export default NavigationBlock;
+
+NavigationBlock.propTypes = {
+    prev: PropTypes.string,
+    next: PropTypes.string,
+
+    prevTitle: PropTypes.string,
+    nextTitle: PropTypes.string,
+};
