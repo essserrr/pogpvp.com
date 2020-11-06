f0df0b3ca777530cc45ab89ad37a5519dd92579b
Essserrr <blizzz93@gmail.com>
2020-11-06 16:34:42 +0900

    Navbar reworked

--- interface/src/App/Navbar/Navbar.jsx
+++ interface/src/App/Navbar/Navbar.jsx
@@ -1,31 +1,32 @@
 import React from "react";
+import LocalizedStrings from "react-localization";
+import { Link } from "react-router-dom";
+import { withRouter } from "react-router";
 
 import ForumIcon from '@material-ui/icons/Forum';
 import LanguageIcon from '@material-ui/icons/Language';
 import TelegramIcon from '@material-ui/icons/Telegram';
 
-import Navbar from "./Navbar/Navbar"
-import DropdownMenu from "App/NavbarWrapper/DropdownMenu/DropdownMenu"
-import NavUser from "./NavUser/NavUser"
-import { Link } from "react-router-dom"
-import { withRouter } from "react-router"
+import NavbarPanel from "./NavbarPanel/NavbarPanel";
+import DropdownMenu from "App/Navbar/DropdownMenu/DropdownMenu";
+import NavUser from "./NavUser/NavUser";
+import Search from "./Search/Search";
 
-import LocalizedStrings from "react-localization"
-import { locale } from "../../locale/locale"
-import { getCookie } from "../../js/getCookie"
-import Search from "./Search/Search"
+import { locale } from "locale/Navbar/Navbar";
+import { getCookie } from "js/getCookie";
 
-import { ReactComponent as En } from "../../icons/us.svg"
-import { ReactComponent as Ru } from "../../icons/ru.svg"
-import { ReactComponent as Battle } from "../../icons/battle.svg"
-import { ReactComponent as Others } from "../../icons/others.svg"
-import { ReactComponent as Redgym } from "../../icons/redgym.svg"
-import { ReactComponent as Dex } from "../../icons/dex.svg"
+import { ReactComponent as En } from "icons/us.svg";
+import { ReactComponent as Ru } from "icons/ru.svg";
+import { ReactComponent as Battle } from "icons/battle.svg";
+import { ReactComponent as Others } from "icons/others.svg";
+import { ReactComponent as Redgym } from "icons/redgym.svg";
+import { ReactComponent as Dex } from "icons/dex.svg";
+import { ReactComponent as Patreon } from "icons/patreon.svg";
 
-let strings = new LocalizedStrings(locale)
+let strings = new LocalizedStrings(locale);
 
 
-class NavbarWrapper extends React.PureComponent {
+class Navbar extends React.PureComponent {
     constructor() {
         super();
         strings.setLanguage(getCookie("appLang") ? getCookie("appLang") : "en")
@@ -41,7 +42,7 @@ class NavbarWrapper extends React.PureComponent {
         const flexStyle = { display: "flex", alignItems: "center" }
 
         return (
-            <Navbar
+            <NavbarPanel
                 leftPanel={[
                     <DropdownMenu key="pvp" icon={<Battle />} label={strings.navbar.pvpTools}>
                         <Link to="/pvp/single">{strings.navbar.single}</Link>
@@ -71,7 +72,7 @@ class NavbarWrapper extends React.PureComponent {
                             <TelegramIcon style={{ fontSize: 32, marginRight: "8px" }} /><span>{strings.navbar.tlg}</span>
                         </a>
                         <a style={flexStyle} title={"Patreon"} href="https://www.patreon.com/pogpvp">
-                            <TelegramIcon style={{ fontSize: 32, marginRight: "8px" }} /><span>{strings.navbar.patr}</span>
+                            <Patreon style={{ width: 32, height: 32, marginRight: "8px" }} /><span>{strings.navbar.patr}</span>
                         </a>
                     </DropdownMenu>,
                     <DropdownMenu key="lang" icon={<LanguageIcon />}>
@@ -89,5 +90,5 @@ class NavbarWrapper extends React.PureComponent {
     }
 }
 
-export default withRouter(NavbarWrapper)
+export default withRouter(Navbar);
 
