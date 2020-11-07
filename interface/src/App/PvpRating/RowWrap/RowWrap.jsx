90c15fa8f4834e10fb91b205cbdf46bf5299fc07
Essserrr <blizzz93@gmail.com>
2020-11-07 15:41:49 +0900

    RowWrap reworked

--- interface/src/App/PvpRating/RowWrap/RowWrap.jsx
+++ interface/src/App/PvpRating/RowWrap/RowWrap.jsx
@@ -1,17 +1,42 @@
-import React from "react"
+import React from "react";
+import PropTypes from 'prop-types';
 
-import "./RowWrap.scss"
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 
-const RowWrap = React.memo(function (props) {
+const useStyles = makeStyles((theme) => ({
+    title: {
+        borderBottom: `0.5px solid ${theme.palette.text.primary}`,
+        fontSize: "1.1em",
+        fontWeight: 400,
+    },
+}));
+
+const RowWrap = React.memo(function RowWrap(props) {
+    const { title, disableIcon, children, ...other } = props
+    const classes = useStyles();
+
     return (
-        <div className={props.outClass}>
-            <div className="row-wrap row justify-content-between mb-1 mx-2 mx-md-3">
-                {props.locale}
-                {!props.disableIcon && <i className="align-self-end fas fa-trophy mr-2 mb-1"></i>}
-            </div>
-            {props.value}
-        </div>
+        <Grid item {...other}>
+            <Grid container justify="center" spacing={2}>
+                <Grid container item xs={12} justify={!disableIcon ? "space-between" : "center"} className={classes.title}>
+                    {title}{!disableIcon && <i className="fas fa-trophy"></i>}
+                </Grid>
+                <Grid item xs={12}>
+                    {children}
+                </Grid>
+            </Grid>
+        </Grid>
     )
 });
 
 export default RowWrap;
+
+RowWrap.propTypes = {
+    title: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.node,
+    ]),
+    disableIcon: PropTypes.bool,
+    children: PropTypes.node,
+};
