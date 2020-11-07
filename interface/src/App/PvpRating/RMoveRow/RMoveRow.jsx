5d3359afb6333c81cfe092753441b7c8040447f6
Essserrr <blizzz93@gmail.com>
2020-11-07 16:09:28 +0900

    ratingMoveRow reworked

--- interface/src/App/PvpRating/RMoveRow/RMoveRow.jsx
+++ interface/src/App/PvpRating/RMoveRow/RMoveRow.jsx
@@ -1,42 +1,86 @@
-import React from "react"
-import ColoredMove from "../../Components/ColoredMove/ColoredMove"
+import React from "react";
+import PropTypes from 'prop-types';
 
-import "./RMoveRow.scss"
+import ColoredMove from "App/Components/ColoredMove/ColoredMove";
 
-const RMoveRow = React.memo(function (props) {
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    rMove: {
+        padding: `${theme.spacing(0.25)}px ${theme.spacing(0.5)}px`,
+        backgroundColor: theme.palette.background.main,
+        borderRadius: `${theme.spacing(0.5)}px`,
+        border: "0.5px solid rgba(0, 0, 0, 0.295)",
+        height: "fit-content",
+        fontWeight: 400,
+
+        position: "relative",
+        "-webkit-transition": 'all 0.15s cubic-bezier(0.165, 0.84, 0.44, 1)',
+        transition: "all 0.15s cubic-bezier(0.165, 0.84, 0.44, 1)",
+
+
+        "&::after": {
+            content: '""',
+            borderRadius: `${theme.spacing(0.5)}px`,
+            position: "absolute",
+            zIndex: -1,
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            boxShadow: "0 5px 15px rgba(0, 0, 0, 0.3)",
+            opacity: 0,
+            "-webkit-transition": "all 0.15s linear",
+            transition: "all 0.15s linear",
+        },
+
+        "&:hover": {
+            "-webkit-transform": "scale(1.05, 1.05)",
+            transform: "scale(1.05, 1.05)",
+        },
+        "&:hover::after": {
+            opacity: 1,
+            display: "block",
+        },
+    }
+}));
+
+const RMoveRow = React.memo(function RMoveRow(props) {
+    const classes = useStyles();
+
     function addStar(moveName) {
         return (props.pokemonTable[props.pokName].EliteMoves[moveName] === 1 ? "*" : "")
     }
 
     return (
-        <div className="rating-moverow row justify-content-between mb-1 mx-2 mx-md-3">
-            <div className="col p-0">
-                <div className="row justify-content-md-left m-0 p-0">
-                    {props.moveTable[props.value.Quick] &&
-                        <ColoredMove m={0.5}
-                            type={props.moveTable[props.value.Quick].MoveType}
-                        >
-                            {props.value.Quick + addStar(props.value.Quick)}
-                        </ColoredMove>}
-                    {props.moveTable[props.value.Charge[0]] &&
-                        <ColoredMove m={0.5}
-                            type={props.moveTable[props.value.Charge[0]].MoveType}
-                        >
-                            {props.value.Charge[0] + addStar(props.value.Charge[0])}
-                        </ColoredMove>}
-                    {props.moveTable[props.value.Charge[1]] &&
-                        <ColoredMove m={0.5}
-                            type={props.moveTable[props.value.Charge[1]].MoveType}
-                        >
-                            {props.value.Charge[1] + addStar(props.value.Charge[1])}
-                        </ColoredMove>}
-                </div>
-            </div>
-            <div className="col-auto text-right align-self-center p-0 pr-2">
+        <Grid container justify="space-between" alignItems="center" className={classes.rMove}>
+            <Grid item container xs>
+                {props.moveTable[props.value.Quick] &&
+                    <ColoredMove m={0.25} type={props.moveTable[props.value.Quick].MoveType}>
+                        {props.value.Quick + addStar(props.value.Quick)}
+                    </ColoredMove>}
+                {props.moveTable[props.value.Charge[0]] &&
+                    <ColoredMove m={0.25} type={props.moveTable[props.value.Charge[0]].MoveType}>
+                        {props.value.Charge[0] + addStar(props.value.Charge[0])}
+                    </ColoredMove>}
+                {props.moveTable[props.value.Charge[1]] &&
+                    <ColoredMove m={0.25} type={props.moveTable[props.value.Charge[1]].MoveType}>
+                        {props.value.Charge[1] + addStar(props.value.Charge[1])}
+                    </ColoredMove>}
+            </Grid>
+            <Grid item xs="auto">
                 {props.value.Rate}
-            </div>
-        </div>
+            </Grid>
+        </Grid>
     )
 });
 
 export default RMoveRow;
+
+RMoveRow.propTypes = {
+    pokName: PropTypes.string,
+    value: PropTypes.object,
+    moveTable: PropTypes.object,
+    pokemonTable: PropTypes.object,
+};
--- interface/src/App/PvpRating/RowWrap/RowWrap.jsx
+++ interface/src/App/PvpRating/RowWrap/RowWrap.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -9,6 +10,7 @@ const useStyles = makeStyles((theme) => ({
         borderBottom: `0.5px solid ${theme.palette.text.primary}`,
         fontSize: "1.1em",
         fontWeight: 400,
+        margin: `${theme.spacing(2)}px 0 ${theme.spacing(2)}px 0`,
     },
 }));
 
@@ -18,10 +20,11 @@ const RowWrap = React.memo(function RowWrap(props) {
 
     return (
         <Grid item {...other}>
-            <Grid container justify="center" spacing={2}>
-                <Grid container item xs={12} justify={!disableIcon ? "space-between" : "center"} className={classes.title}>
+            <Grid container justify="center">
+                <Grid container item xs={12} justify={!disableIcon ? "space-between" : "center"} alignItems="center" className={classes.title}>
                     {title}{!disableIcon && <i className="fas fa-trophy"></i>}
                 </Grid>
+                <Divider />
                 <Grid item xs={12}>
                     {children}
                 </Grid>
