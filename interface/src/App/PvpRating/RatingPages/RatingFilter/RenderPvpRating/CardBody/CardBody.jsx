40906c84a7c3622292c17685fb04796e5feae62c
Essserrr <blizzz93@gmail.com>
2020-11-07 14:56:30 +0900

    Raiting card body reworked

--- interface/src/App/PvpRating/RatingPages/RatingFilter/RenderPvpRating/CardBody/CardBody.jsx
+++ interface/src/App/PvpRating/RatingPages/RatingFilter/RenderPvpRating/CardBody/CardBody.jsx
@@ -1,47 +1,78 @@
-import React from "react"
-import LocalizedStrings from "react-localization"
+import React from "react";
+import LocalizedStrings from "react-localization";
+import PropTypes from 'prop-types';
 
-import Iconer from "App/Components/Iconer/Iconer"
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Iconer from "App/Components/Iconer/Iconer";
+
 import { getCookie } from "js/getCookie"
 import { locale } from "locale/locale"
 
-import "./CardBody.scss"
+const useStyles = makeStyles((theme) => ({
+    secondIcon: {
+        marginLeft: `${theme.spacing(1)}px`,
+    },
+    body: {
+        padding: `${theme.spacing(1)}px`,
+    },
+    score: {
+        backgroundColor: theme.palette.background.main,
+        borderRadius: "5px",
+        border: "0.5px solid rgba(0, 0, 0, 0.295)",
+        height: "fit-content",
+
+        fontSize: "1.2rem",
+        fontWeight: "bold",
+    },
+}));
 
 let strings = new LocalizedStrings(locale)
 
 const CardBody = React.memo(function (props) {
     strings.setLanguage(getCookie("appLang") ? getCookie("appLang") : "en")
+    const classes = useStyles();
 
-
+    const pokemon = props.pokemonTable[props.name]
     return (
-        <div className="row justify-content-between align-items-center m-0">
-            <div className="ratingcard-body--font col p-0">
-                <div className="row m-0 align-items-center">
-                    <span className="mr-2">
+        <Grid container justify="center" alignItems="center" spacing={1} className={classes.body}>
+
+            <Grid item xs>
+                <Grid container justify="flex-start" alignItems="center">
+                    <Box component="span" mr={1} >
                         {strings.rating.type}
-                    </span>
-                    {(props.pokemonTable[props.name].Type[0] !== undefined) &&
-                        <Iconer
-                            className={"ratingcard-body__icon"}
-                            folderName="/type/"
-                            fileName={props.pokemonTable[props.name].Type[0]}
+                    </Box>
+                    {(pokemon.Type[0] !== undefined) &&
+                        <Iconer size={18}
+                            folderName="/type/" fileName={String(pokemon.Type[0])}
                         />}
-                    {(props.pokemonTable[props.name].Type[1] !== undefined) &&
-                        <Iconer
-                            className={"ratingcard-body__icon ml-2"}
-                            folderName="/type/"
-                            fileName={props.pokemonTable[props.name].Type[1]}
+                    {(pokemon.Type[1] !== undefined) &&
+                        <Iconer className={classes.secondIcon} size={18}
+                            folderName="/type/" fileName={String(pokemon.Type[1])}
                         />}
-                </div>
-                {strings.rating.avgRate + " " + props.entry.AvgRate} <br />
-                {strings.rating.avgWin + " " + (props.entry.AvgWinrate * 100).toFixed(0) + "%"}
-            </div>
-            <div className="ratingcard-body__score col-auto text-center px-2 mx-2">
+
+                    <Grid item xs={12}>{`${strings.rating.avgRate} ${props.entry.AvgRate}`}</Grid>
+                    <Grid item xs={12}>{`${strings.rating.avgWin} ${(props.entry.AvgWinrate * 100).toFixed(0)}%`}</Grid>
+                </Grid>
+            </Grid>
+
+            <Grid item xs="auto" className={classes.score}>
                 {strings.rating.score}<br />
                 {(props.entry.AvgRateWeighted / props.maxWeighted * 100).toFixed(1)}
-            </div>
-        </div>
+            </Grid>
+
+        </Grid>
     )
 });
 
 export default CardBody;
+
+CardBody.propTypes = {
+    name: PropTypes.string.isRequired,
+    pokemonTable: PropTypes.object.isRequired,
+
+    maxWeighted: PropTypes.number.isRequired,
+    entry: PropTypes.object.isRequired,
+};
