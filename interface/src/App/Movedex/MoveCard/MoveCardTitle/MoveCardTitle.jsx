dc751d460a4310b2559ca1ee33aaf6ba64c11ef5
Essserrr <blizzz93@gmail.com>
2020-11-10 09:43:14 +0900

    MoveCard title reworked

--- interface/src/App/Movedex/MoveCard/MoveCardTitle/MoveCardTitle.jsx
+++ interface/src/App/Movedex/MoveCard/MoveCardTitle/MoveCardTitle.jsx
@@ -1,34 +1,53 @@
-import React from "react"
-import LocalizedStrings from "react-localization"
+import React from "react";
+import LocalizedStrings from "react-localization";
+import PropTypes from 'prop-types';
 
-import { getCookie } from "js/getCookie"
-import { dexLocale } from "locale/dexLocale"
-import Iconer from "App/Components/Iconer/Iconer"
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
-import "./MoveCardTitle.scss"
+import Iconer from "App/Components/Iconer/Iconer";
+import { getCookie } from "js/getCookie";
+import { dexLocale } from "locale/Movedex/Movecard";
 
-let strings = new LocalizedStrings(dexLocale)
+
+const useStyles = makeStyles((theme) => ({
+    title: {
+        fontWeight: 500,
+        borderBottom: "3px solid rgba(0, 0, 0, 0.342)",
+    },
+}));
+
+let strings = new LocalizedStrings(dexLocale);
 
 const MoveCardTitle = React.memo(function (props) {
     strings.setLanguage(getCookie("appLang") ? getCookie("appLang") : "en")
+    const classes = useStyles();
 
     return (
-        <div className="movecard-title row m-0 p-0 pb-1 mb-2">
-            <Iconer
-                className={"ml-2  mr-1 icon24 align-self-center"}
-                size={24}
-                folderName="/type/"
-                fileName={props.move.MoveType}
-            />
-            <h3 className={"align-self-center m-0"}>
-                {props.move.Title}
-            </h3>
-            <h5 className={"align-self-center m-0 ml-1"}>
-                ({props.move.MoveCategory === "Charge Move" ? strings.chm : strings.qm})
-            </h5>
-        </div>
+        <Grid container alignItems="center" className={classes.title}>
+
+            <Iconer size={24} folderName="/type/" fileName={String(props.move.MoveType)} />
+
+            <Box ml={1}>
+                <Typography variant="h4">
+                    {props.move.Title}
+                </Typography>
+            </Box>
+
+            <Box ml={1}>
+                <Typography variant="h5">
+                    ({props.move.MoveCategory === "Charge Move" ? strings.chm : strings.qm})
+                </Typography>
+            </Box>
+        </Grid>
     )
 
 });
 
 export default MoveCardTitle;
+
+MoveCardTitle.propTypes = {
+    move: PropTypes.object.isRequired,
+};
