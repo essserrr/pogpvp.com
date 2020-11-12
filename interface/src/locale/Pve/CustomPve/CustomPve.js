1d3ed9a53c29f0eaa9ae4d3ef6112c826e9a06c7
Essserrr <blizzz93@gmail.com>
2020-11-12 12:13:21 +0900

    CustomPvePlayer reworked

---
+++ interface/src/locale/Pve/CustomPve/CustomPve.js
@@ -0,0 +1,13 @@
+export const pveLocale = {
+    en: {
+        player: "Player",
+        party: "Group",
+        none: "None",
+    },
+
+    ru: {
+        player: "Игрок",
+        party: "Группа",
+        none: "Нет",
+    }
+}
