845f299878305eb89b114213ea14122d7c109f15
Essserrr <blizzz93@gmail.com>
2020-11-12 12:47:58 +0900

    Title is title again

--- interface/src/App/Components/SearchableSelect/SearchableSelect.jsx
+++ interface/src/App/Components/SearchableSelect/SearchableSelect.jsx
@@ -29,7 +29,7 @@ const SearchableSelect = React.memo(function SearchableSelect(props) {
             name={name}
 
             options={props.children}
-            getOptionLabel={(option) => typeof option === 'string' ? option : option.value}
+            getOptionLabel={(option) => typeof option === 'string' ? option : option.title}
             getOptionSelected={(option, value) => (typeof option === 'string' ? option : option.value) === (typeof value === 'string' ? value : value.value)}
             onChange={(event, ...otherOptions) => onChange(event, { type: type, name: name, category: category, attr: attr, }, ...otherOptions)}
             renderInput={(params) => <TextField {...params} label={label} helperText={selectedText} error={!!errorText} />}
