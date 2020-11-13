40e12e54aa90922186e8fef686c848ed958fde01
Essserrr <blizzz93@gmail.com>
2020-11-13 18:13:03 +0900

    DetailedStatGenerator reworked

--- interface/src/App/PvE/Components/PveResult/PlayerResProcessor/PlayerResEntry/DetailedStatisticsGenerator/DetailedStatisticsGenerator.jsx
+++ interface/src/App/PvE/Components/PveResult/PlayerResProcessor/PlayerResEntry/DetailedStatisticsGenerator/DetailedStatisticsGenerator.jsx
@@ -1,19 +1,29 @@
-import React from "react"
-import DetailedWrapper from "./DetailedWrapper/DetailedWrapper"
+import React from "react";
+import PropTypes from 'prop-types';
 
-const DetailedStatisticsGenerator = React.memo(function (props) {
+import DetailedWrapper from "./DetailedWrapper/DetailedWrapper";
+
+const DetailedStatisticsGenerator = React.memo(function DetailedStatisticsGenerator(props) {
+    const { value, ...other } = props;
+
     return (
-        Object.entries(props.value).map(detailed =>
+        Object.entries(value).map(detailed =>
             <DetailedWrapper
                 key={detailed[0]}
-                {...props}
                 value={detailed[1]}
                 disabled={{ avg: true, max: true, min: true, }}
+                {...other}
             />
         )
     )
 });
 
-
-
 export default DetailedStatisticsGenerator;
+
+DetailedStatisticsGenerator.propTypes = {
+    value: PropTypes.array,
+
+    tables: PropTypes.object,
+    moveTable: PropTypes.object,
+    snapshot: PropTypes.object,
+};
