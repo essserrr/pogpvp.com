5bac7ad1ccdc0a02947fda9f3196f2083557eb69
Essserrr <blizzz93@gmail.com>
2020-11-15 22:06:13 +0900

    HeaDrEWORKED

---
+++ interface/src/App/PvP/components/SinglePvpResults/Head.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
+const Head = React.memo(function Head(props) {
+    return (
+        <TableHead className="thead" >
+            <TableRow>
+                <TableCell component="th" scope="col"></TableCell>
+                <TableCell component="th" scope="col" align="center">{props.NameA}</TableCell>
+                <TableCell component="th" scope="col" align="center">{props.NameD}</TableCell>
+            </TableRow>
+        </TableHead>
+    )
+});
+
+export default Head;
+
+Head.propTypes = {
+    NameA: PropTypes.string,
+    NameD: PropTypes.string,
+};
--- interface/src/App/PvP/components/SinglePvpResults/Line.jsx
+++ interface/src/App/PvP/components/SinglePvpResults/Line.jsx
@@ -7,9 +7,9 @@ import TableCell from '@material-ui/core/TableCell';
 const Line = React.memo(function Line(props) {
     return (
         <TableRow>
-            <TableCell component="th">{props.title}</TableCell>
-            <TableCell>{props.valueA}</TableCell>
-            <TableCell>{props.valueD}</TableCell>
+            <TableCell component="th" style={{ maxWidth: "160px" }}>{props.title}</TableCell>
+            <TableCell align="center">{props.valueA}</TableCell>
+            <TableCell align="center">{props.valueD}</TableCell>
         </TableRow>
     )
 });
