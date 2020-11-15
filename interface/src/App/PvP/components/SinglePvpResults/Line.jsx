acc30d7068f7c94d07211e4af1feba7ceef5a5b3
Essserrr <blizzz93@gmail.com>
2020-11-15 22:01:48 +0900

    Line reworked

--- interface/src/App/PvP/components/SinglePvpResults/Line.jsx
+++ interface/src/App/PvP/components/SinglePvpResults/Line.jsx
@@ -1,14 +1,29 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
-const Line = React.memo(function (props) {
+const Line = React.memo(function Line(props) {
     return (
-        <tr>
-            <th style={{ maxWidth: "140px" }} className="m-0 p-0" scope="row" >{props.title}</th>
-            <td className="m-0 p-0" >{props.valueA}</td>
-            <td className="m-0 p-0" >{props.valueD}</td>
-        </tr>
+        <TableRow>
+            <TableCell component="th">{props.title}</TableCell>
+            <TableCell>{props.valueA}</TableCell>
+            <TableCell>{props.valueD}</TableCell>
+        </TableRow>
     )
 });
 
 export default Line;
+
+Line.propTypes = {
+    title: PropTypes.string,
+    valueA: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.node,
+    ]),
+    valueD: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.node,
+    ]),
+};
