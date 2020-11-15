5e4c680bfa19ba3edb63c834edfdd58199c43e9c
Essserrr <blizzz93@gmail.com>
2020-11-15 10:49:54 +0900

    Pokemon reworked

--- interface/src/App/PvP/components/Pokemon.jsx
+++ interface/src/App/PvP/components/Pokemon.jsx
@@ -1,122 +1,116 @@
-import React from "react"
+import React from "react";
+import LocalizedStrings from "react-localization";
+import PropTypes from 'prop-types';
 
 import MenuItem from '@material-ui/core/MenuItem';
 import Grid from '@material-ui/core/Grid';
 
 import WithIcon from "App/Components/WithIcon/WithIcon";
 import Input from "App/Components/Input/Input";
-
+import GreyPaper from 'App/Components/GreyPaper/GreyPaper';
 import Stats from "App/Components/Stats/Stats";
 import SearchableSelect from 'App/Components/SearchableSelect/SearchableSelect';
-import Stages from "./Stages/Stages"
-import InitialStats from "./InitialStats/InitialStats"
-import Maximizer from "./Maximizer/Maximizer"
-import CpAndTyping from "App/Components/CpAndTypes/CpAndTypes"
-import EffectiveStats from "./EffectiveStats/EffectiveStats"
-import MagicBox from "./MagicBox/MagicBox"
-import PokemonSelect from "../../Userpage/CustomPokemon/PartyBox/PokemonSelect/PokemonSelect"
+import Stages from "./Stages/Stages";
+import InitialStats from "./InitialStats/InitialStats";
+import Maximizer from "./Maximizer/Maximizer";
+import CpAndTyping from "App/Components/CpAndTypes/CpAndTypes";
+import EffectiveStats from "./EffectiveStats/EffectiveStats";
+import MagicBox from "./MagicBox/MagicBox";
+import PokemonSelect from "App/Userpage/CustomPokemon/PartyBox/PokemonSelect/PokemonSelect";
 import MoveTip from "./MoveTip/MoveTip";
-
 import MoveSelect from "App/Components/MoveSelect/MoveSelect";
 
-import LocalizedStrings from "react-localization"
 import { pvp } from "locale/Pvp/Pvp";
 import { getCookie } from "js/getCookie";
 import { options } from "locale/Components/Options/locale";
 
-import "./Pokemon.scss"
-
 let strings = new LocalizedStrings(pvp);
 let optionStrings = new LocalizedStrings(options);
 
-class Pokemon extends React.PureComponent {
-    constructor(props) {
-        super(props);
-        strings.setLanguage(getCookie("appLang") ? getCookie("appLang") : "en");
-        optionStrings.setLanguage(getCookie("appLang") ? getCookie("appLang") : "en");
-    }
+const Pokemon = React.memo(function Pokemon(props) {
+    strings.setLanguage(getCookie("appLang") ? getCookie("appLang") : "en");
+    optionStrings.setLanguage(getCookie("appLang") ? getCookie("appLang") : "en");
 
-    render() {
+    return (
+        <GreyPaper elevation={4} enablePadding paddingMult={0.5}>
+            <Grid container spacing={1}>
 
-        return (
-            <Grid container className={`pokemon ${this.props.className ? this.props.className : ""}`} spacing={1}>
-
-                <MagicBox open={Boolean(this.props.showMenu)} onClick={this.props.onClick} attr={this.props.attr}>
-                    <SearchableSelect disableClearable label={"fullLabel"} name={this.props.category}
-                        attr={this.props.attr} onChange={this.props.onChange}>
-                        {this.props.moveList}
+                <MagicBox open={Boolean(props.showMenu)} onClick={props.onClick} attr={props.attr}>
+                    <SearchableSelect disableClearable label={"fullLabel"} name={props.category}
+                        attr={props.attr} onChange={props.onChange}>
+                        {props.moveList}
                     </SearchableSelect>
                 </MagicBox>
 
-                {this.props.userPokemon && this.props.userPokemon.length > 0 &&
+                {props.userPokemon && props.userPokemon.length > 0 &&
                     <Grid item xs={12}>
-                        <PokemonSelect label={strings.userPok} pokemonTable={this.props.pokemonTable}
-                            attr={this.props.attr} name="userPokemon" onChange={this.props.onChange}>
-                            {this.props.userPokemon}
+                        <PokemonSelect label={strings.userPok} pokemonTable={props.pokemonTable}
+                            attr={props.attr} name="userPokemon" onChange={props.onChange}>
+                            {props.userPokemon}
                         </PokemonSelect>
                     </Grid>}
 
-                {this.props.pokList &&
+                {props.pokList &&
                     <Grid item xs={12}>
                         <SearchableSelect disableClearable label={strings.allPok}
-                            value={this.props.value.name} attr={this.props.attr} name="Name" onChange={this.props.onChange}>
-                            {this.props.pokList}
+                            value={props.value.name} attr={props.attr} name="Name" onChange={props.onChange}>
+                            {props.pokList}
                         </SearchableSelect>
                     </Grid>}
 
-                {(this.props.pokemonTable[this.props.value.name] && this.props.value.name) &&
+                {(props.pokemonTable[props.value.name] && props.value.name) &&
                     <>
                         <Grid item xs={12}>
                             <CpAndTyping
-                                Lvl={this.props.value.Lvl}
-                                Atk={this.props.value.Atk}
-                                Def={this.props.value.Def}
-                                Sta={this.props.value.Sta}
+                                Lvl={props.value.Lvl}
+                                Atk={props.value.Atk}
+                                Def={props.value.Def}
+                                Sta={props.value.Sta}
 
-                                isShadow={this.props.value.IsShadow === "true"}
+                                isShadow={props.value.IsShadow === "true"}
 
-                                pokemonTable={this.props.pokemonTable}
-                                name={this.props.value.name}
+                                pokemonTable={props.pokemonTable}
+                                name={props.value.name}
                             />
                         </Grid>
 
                         <Grid item xs={12}>
                             <EffectiveStats
-                                effAtk={this.props.value.effAtk}
-                                effDef={this.props.value.effDef}
-                                effSta={this.props.value.effSta}
-                                AtkStage={Number(this.props.value.AtkStage)}
-                                DefStage={Number(this.props.value.DefStage)}
-                                attr={this.props.attr}
+                                effAtk={props.value.effAtk}
+                                effDef={props.value.effDef}
+                                effSta={props.value.effSta}
+                                AtkStage={Number(props.value.AtkStage)}
+                                DefStage={Number(props.value.DefStage)}
+                                attr={props.attr}
                             />
                         </Grid>
 
                         <Grid item xs={12}>
                             <Stats
-                                Lvl={String(this.props.value.Lvl)}
-                                Atk={String(this.props.value.Atk)}
-                                Def={String(this.props.value.Def)}
-                                Sta={String(this.props.value.Sta)}
-                                attr={this.props.attr}
-                                onChange={this.props.onChange}
+                                Lvl={String(props.value.Lvl)}
+                                Atk={String(props.value.Atk)}
+                                Def={String(props.value.Def)}
+                                Sta={String(props.value.Sta)}
+                                attr={props.attr}
+                                onChange={props.onChange}
                             />
                         </Grid>
 
                         <Grid item xs={12}>
                             <Maximizer
-                                attr={this.props.attr}
+                                attr={props.attr}
                                 category={"defaultStatMaximizer"}
-                                value={this.props.value.maximizer}
-                                onChange={this.props.onChange}
+                                value={props.value.maximizer}
+                                onChange={props.onChange}
                             />
                         </Grid>
 
                         <Grid item xs={12}>
                             <InitialStats
-                                InitialHP={this.props.value.InitialHP}
-                                InitialEnergy={this.props.value.InitialEnergy}
-                                attr={this.props.attr}
-                                onChange={this.props.onChange}
+                                InitialHP={props.value.InitialHP}
+                                InitialEnergy={props.value.InitialEnergy}
+                                attr={props.attr}
+                                onChange={props.onChange}
 
                                 label={strings.title.initialStats}
                             />
@@ -124,16 +118,16 @@ class Pokemon extends React.PureComponent {
 
                         <Grid item xs={12}>
                             <Stages
-                                Atk={Number(this.props.value.AtkStage)}
-                                Def={Number(this.props.value.DefStage)}
-                                attr={this.props.attr}
-                                onChange={this.props.onChange}
+                                Atk={Number(props.value.AtkStage)}
+                                Def={Number(props.value.DefStage)}
+                                attr={props.attr}
+                                onChange={props.onChange}
                             />
                         </Grid>
 
                         <Grid item xs={12}>
-                            <Input select name="Shields" value={this.props.value.Shields}
-                                attr={this.props.attr} label={strings.title.shields} onChange={this.props.onChange}>
+                            <Input select name="Shields" value={props.value.Shields}
+                                attr={props.attr} label={strings.title.shields} onChange={props.onChange}>
                                 <MenuItem value="0">0</MenuItem>
                                 <MenuItem value="1">1</MenuItem>
                                 <MenuItem value="2">2</MenuItem>
@@ -142,8 +136,8 @@ class Pokemon extends React.PureComponent {
 
                         <Grid item xs={12}>
                             <WithIcon tip={<>{strings.tips.strategy.greedy}<br /><br />{strings.tips.strategy.shieldSaving}</>}>
-                                <Input select name="IsGreedy" value={this.props.value.IsGreedy}
-                                    attr={this.props.attr} label={strings.title.strategy} onChange={this.props.onChange}>
+                                <Input select name="IsGreedy" value={props.value.IsGreedy}
+                                    attr={props.attr} label={strings.title.strategy} onChange={props.onChange}>
                                     <MenuItem value="true">{optionStrings.options.strategy.greedy}</MenuItem>
                                     <MenuItem value="false">{optionStrings.options.strategy.shieldSaving}</MenuItem>
                                 </Input>
@@ -152,8 +146,8 @@ class Pokemon extends React.PureComponent {
 
                         <Grid item xs={12}>
                             <WithIcon tip={strings.tips.shadow}>
-                                <Input select name="IsShadow" value={this.props.value.IsShadow}
-                                    attr={this.props.attr} label={strings.title.type} onChange={this.props.onChange}>
+                                <Input select name="IsShadow" value={props.value.IsShadow}
+                                    attr={props.attr} label={strings.title.type} onChange={props.onChange}>
                                     <MenuItem value="false">{optionStrings.options.type.normal}</MenuItem>
                                     <MenuItem value="true">{optionStrings.options.type.shadow}</MenuItem>
                                 </Input>
@@ -161,55 +155,72 @@ class Pokemon extends React.PureComponent {
                         </Grid>
 
                         <Grid item xs={12}>
-                            <MoveSelect name="QuickMove" attr={this.props.attr} onChange={this.props.onChange}
-                                value={this.props.value.QuickMove &&
-                                    this.props.moveTable[this.props.value.QuickMove] !== undefined && this.props.value.QuickMove}
-                                moveType={this.props.moveTable[this.props.value.QuickMove] !== undefined ?
-                                    this.props.moveTable[this.props.value.QuickMove].MoveType : ""}
+                            <MoveSelect name="QuickMove" attr={props.attr} onChange={props.onChange}
+                                value={props.value.QuickMove &&
+                                    props.moveTable[props.value.QuickMove] !== undefined && props.value.QuickMove}
+                                moveType={props.moveTable[props.value.QuickMove] !== undefined ?
+                                    props.moveTable[props.value.QuickMove].MoveType : ""}
 
                                 label={strings.title.quickMove}
-                                tip={<MoveTip moveName={this.props.value.QuickMove} moveTable={this.props.moveTable} />}
+                                tip={<MoveTip moveName={props.value.QuickMove} moveTable={props.moveTable} />}
                             >
-                                {this.props.value.quickMovePool}
+                                {props.value.quickMovePool}
                             </MoveSelect>
                         </Grid>
 
                         <Grid item xs={12}>
                             <MoveSelect
-                                value={(this.props.value.ChargeMove1 && this.props.value.ChargeMove1 !== "Select..." &&
-                                    this.props.moveTable[this.props.value.ChargeMove1] !== undefined) ? this.props.value.ChargeMove1 : ""}
-                                moveType={(this.props.moveTable[this.props.value.ChargeMove1] !== undefined) ?
-                                    this.props.moveTable[this.props.value.ChargeMove1].MoveType : ""}
+                                value={(props.value.ChargeMove1 && props.value.ChargeMove1 !== "Select..." &&
+                                    props.moveTable[props.value.ChargeMove1] !== undefined) ? props.value.ChargeMove1 : ""}
+                                moveType={(props.moveTable[props.value.ChargeMove1] !== undefined) ?
+                                    props.moveTable[props.value.ChargeMove1].MoveType : ""}
 
-                                name="ChargeMove1" attr={this.props.attr} onChange={this.props.onChange}
+                                name="ChargeMove1" attr={props.attr} onChange={props.onChange}
 
                                 label={strings.title.chargeMove}
-                                tip={<MoveTip moveName={this.props.value.ChargeMove1} moveTable={this.props.moveTable} />}
+                                tip={<MoveTip moveName={props.value.ChargeMove1} moveTable={props.moveTable} />}
                             >
-                                {this.props.value.chargeMovePool}
+                                {props.value.chargeMovePool}
                             </MoveSelect>
                         </Grid>
 
                         <Grid item xs={12}>
                             <MoveSelect
-                                value={(this.props.value.ChargeMove2 && this.props.moveTable[this.props.value.ChargeMove2] !== undefined &&
-                                    this.props.value.ChargeMove2 !== "Select...") ? this.props.value.ChargeMove2 : ""}
-                                moveType={(this.props.moveTable[this.props.value.ChargeMove2] !== undefined) ?
-                                    this.props.moveTable[this.props.value.ChargeMove2].MoveType : ""}
+                                value={(props.value.ChargeMove2 && props.moveTable[props.value.ChargeMove2] !== undefined &&
+                                    props.value.ChargeMove2 !== "Select...") ? props.value.ChargeMove2 : ""}
+                                moveType={(props.moveTable[props.value.ChargeMove2] !== undefined) ?
+                                    props.moveTable[props.value.ChargeMove2].MoveType : ""}
 
-                                name="ChargeMove2" attr={this.props.attr} onChange={this.props.onChange}
+                                name="ChargeMove2" attr={props.attr} onChange={props.onChange}
 
                                 label={strings.title.chargeMove}
-                                tip={<MoveTip moveName={this.props.value.ChargeMove2} moveTable={this.props.moveTable} />}
+                                tip={<MoveTip moveName={props.value.ChargeMove2} moveTable={props.moveTable} />}
                             >
-                                {this.props.value.chargeMovePool}
+                                {props.value.chargeMovePool}
                             </MoveSelect>
                         </Grid>
                     </>}
             </Grid>
-        )
-    }
+        </GreyPaper>
+    )
+});
 
-}
+export default Pokemon;
 
-export default Pokemon
+Pokemon.propTypes = {
+    value: PropTypes.object,
+    attr: PropTypes.string,
+
+    pokemonTable: PropTypes.object,
+    moveTable: PropTypes.object,
+    moveList: PropTypes.arrayOf(PropTypes.object),
+    pokList: PropTypes.arrayOf(PropTypes.object),
+    userPokemon: PropTypes.arrayOf(PropTypes.object),
+
+    showMenu: PropTypes.bool,
+    category: PropTypes.bool,
+
+    onChange: PropTypes.func,
+    statMaximizer: PropTypes.func,
+    onClick: PropTypes.func,
+};
